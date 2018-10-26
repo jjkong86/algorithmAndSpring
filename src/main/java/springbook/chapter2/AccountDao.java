99701b4c909475e6d86945b6609bc9fd227fc93f
정재공 <jeagong86@ibizsoftware.net>
2018-10-26 10:27:37 +0900

    test ddd

--- src/main/java/springbook/chapter2/AccountDao.java
+++ src/main/java/springbook/chapter2/AccountDao.java
@@ -1,11 +1,7 @@
 package springbook.chapter2;
 //test a
 //test b
-<<<<<<< HEAD
 //test ddd
-=======
->>>>>>> branch 'master' of https://github.com/jjkong86/myTestSpring
-//test ff
 public class AccountDao {
 
 	private ConnectionMaker connectionMaker;
