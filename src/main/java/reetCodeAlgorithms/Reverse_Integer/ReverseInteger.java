11c232b80b8c0bba2ba1544f17c5de3488c5b8a9
정재공 <jeagong86@ibizsoftware.net>
2018-11-22 19:40:57 +0900

    no message

--- src/main/java/reetCodeAlgorithms/Reverse_Integer/ReverseInteger.java
+++ src/main/java/reetCodeAlgorithms/Reverse_Integer/ReverseInteger.java
@@ -2,21 +2,19 @@ package reetCodeAlgorithms.Reverse_Integer;
 
 public class ReverseInteger {
 	public int reverse(int x) {
-		String ret = String.valueOf(x);
-		ret = new StringBuilder(ret).reverse().toString();
-		long retInt = 0L;
-        if(x < 0) {
-        	ret = ret.replace("-", "");
-        	retInt = Long.valueOf(ret)*(-1);
-        } else {
-        	retInt = Long.valueOf(ret);
+		int ret = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            x /= 10;
+            if (ret > Integer.MAX_VALUE/10 || (ret == Integer.MAX_VALUE / 10 && pop > 7)) return 0;
+            if (ret < Integer.MIN_VALUE/10 || (ret == Integer.MIN_VALUE / 10 && pop < -8)) return 0;
+            ret = ret * 10 + pop;
         }
-		
-		return (int) retInt;	
+        return ret;
 	}
 	
 	public static void main(String[] args) {
-		int x = 2147483647;
+		int x = 1534236469;
 		ReverseInteger r = new ReverseInteger();
 		System.out.println(r.reverse(x));
 	}
---
+++ src/main/java/reetCodeAlgorithms/atoi/AtoISolution.java
@@ -0,0 +1,48 @@
+package reetCodeAlgorithms.atoi;
+
+public class AtoISolution {
+	
+	public int myAtoi(String str) {
+		
+		return 0;
+    } 
+	
+	public static void main(String[] args) {
+		String str = "419332222222222222222222222222  ";
+		int ret = 0;
+		boolean flag = true;
+		for (int i = 0; i < str.length(); i++) {
+			String tempStr = "";
+			tempStr = str.substring(i, i+1);
+			
+//			if(ret > 0 && tempStr.equals(" ")) {
+//				break;
+//			}
+			if (!tempStr.equals(" ") && !tempStr.equals("-")) {
+				
+				try {
+					if(ret*10 + Integer.valueOf(tempStr) > Integer.MAX_VALUE/10) {
+						ret = Integer.MAX_VALUE;
+						break;
+					} else if (!flag && ret*10 + Integer.valueOf(tempStr)*(-1) < Integer.MIN_VALUE/10) {
+						ret = Integer.MIN_VALUE*10;
+						break;
+					}
+					ret = ret*10 + Integer.valueOf(tempStr);
+					int tempI = ret;
+					System.out.println(tempI *= flag ? 1 : -1);
+				} catch (NumberFormatException e) {
+					break;
+				}
+			} else if (tempStr.equals("-")) {
+				flag = false;
+			}
+		}
+		if (!flag) {
+			ret *= (-1);
+		}
+		
+		System.out.println(ret + "::"+flag);
+	}
+
+}
