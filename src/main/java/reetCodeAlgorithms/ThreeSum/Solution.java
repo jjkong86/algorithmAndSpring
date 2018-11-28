de9fd00010dcfe62fba1afc649dc07e6778e2406
정재공 <jeagong86@ibizsoftware.net>
2018-11-28 18:45:22 +0900

    no message

--- src/main/java/reetCodeAlgorithms/ThreeSum/Solution.java
+++ src/main/java/reetCodeAlgorithms/ThreeSum/Solution.java
@@ -2,21 +2,34 @@ package reetCodeAlgorithms.ThreeSum;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Solution {
 	
 	public List<List<Integer>> threeSum(int[] nums) {
 		List<Object> ret = new ArrayList<>();
-		int sum = 0;
-		List<Integer> list = Arrays.stream(nums).boxed().collect(Collectors.toList());
+//		List<Integer> numsList = Arrays.stream(nums).boxed().collect(Collectors.toList());
+		Map<String, Integer> numsMap = new HashMap<>();
+		for (Integer integer : nums) {
+			String numStr = String.valueOf(integer);
+			numsMap.put(numStr, integer);
+		}
 		
 		for (int i = 0; i < nums.length; i++) {
 			int firstNum = nums[i];
 			for (int j = i+1; j < nums.length; j++) {
 				int SeNum = nums[j];
-				if (firstNum + SeNum == list.get((firstNum + SeNum)*(-1))) {
+				String key = String.valueOf((firstNum + SeNum)*(-1));
+				System.out.println(key);
+				if (key == null) {
+					System.out.println("key null");
+					continue;
+				}
+				int sol = numsMap.get(key);
+				if (firstNum + SeNum == sol) {
 					List<Integer> tempRet = new ArrayList<>();
 					tempRet.add(firstNum);
 					tempRet.add(SeNum);
@@ -26,7 +39,7 @@ public class Solution {
 				
 			}
 		}
-		System.out.println(sum);
+		System.out.println(ret);
 		return null;
     }
 	
@@ -34,6 +47,13 @@ public class Solution {
 		Solution s = new Solution();
 		int[] nums = {-1, 0, 1, 2, -1, -4};
 		s.threeSum(nums);
+		Map<String, Integer> numsMap = new HashMap<>();
+		for (Integer integer : nums) {
+			String numStr = String.valueOf(integer);
+			numsMap.put(numStr, integer);
+		}
+//		System.out.println(numsMap.get("5"));
+		
 	}
 
 }
