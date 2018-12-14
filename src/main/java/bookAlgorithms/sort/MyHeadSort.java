c327ac2cdbfb47c8bb0298b23efe72b54f76eb70
정재공 <jeagong86@ibizsoftware.net>
2018-12-14 10:12:00 +0900

    1.힙을 이용하여 오름차순 정렬 2. 먼저 힙을 내림차순으로 빌드함(힙의 속성을 만족해야함) 3. 그리고 최초의 루트 노드와 맨끝에 있는 원소와 교환하고 다시 힙정렬 -> ex array[0] <-> array[9] ---> array[0] <-> array[8] .... ---> 정렬 끝

--- src/main/java/bookAlgorithms/sort/MyHeadSort.java
+++ src/main/java/bookAlgorithms/sort/MyHeadSort.java
@@ -4,6 +4,11 @@ import java.util.Random;
 
 public class MyHeadSort {
 	public static void main(String[] args) {
+		/*
+		 * 힙을 이용하여 오름차순 정렬
+		 * 먼저 힙을 내림차순으로 빌드함(힙의 속성을 만족해야함)
+		 * 그리고 최초의 루트 노드와 맨끝에 있는 원소와 교환하고 다시 힙정렬 -> ex array[0] <-> array[9] ---> array[0] <-> array[8] .... ---> 정렬 끝
+		 */
 		Random gen =  new Random();
 		int[] array = new int[10];
 		for (int i = 0; i < 10; i++) {
@@ -17,27 +22,17 @@ public class MyHeadSort {
 		System.out.println("정렬 전 : ["+print+"]");
 		
 		MyHeadSort s = new MyHeadSort();
-		s.sort(array);
-	}
-
-	private void sort(int[] array) {
-		int len = array.length;
+		array = s.sort(array, array.length-1);
 		
-		for (int i = 1; i < len; i++) {
-			int child = i;
+		for (int i = array.length-1; i >= 0 ; i--) {
+			int temp = array[i];
+			array[i] = array[0];
+			array[0] = temp;
 			
-			while (child > 0) {
-				int parent = child/2;
-				if (array[parent] > array[child]) {
-					int temp = array[parent];
-					array[parent] = array[child];
-					array[child] = temp;
-				}
-				child = parent;
-			}
+			s.sort(array, i);
 		}
 		
-		StringBuilder print = new StringBuilder("정렬 후 : ["+array[0]+", ");
+		print = new StringBuilder("정렬 후 : ["+array[0]+", ");
 		for (int j = 1; j < array.length; j++) {
 			print.append(array[j]).append(j == array.length-1 ? "" : ", ");
 			if (array[j-1] > array[j]) {
@@ -45,5 +40,23 @@ public class MyHeadSort {
 			}
 		}
 		System.out.println(print+"]");
+	}
+
+	private int[] sort(int[] array, int len) {
+		
+		for (int i = 1; i < len; i++) {
+			int child = i;
+			
+			while (child > 0) {
+				int parent = child/2;
+				if (array[child] > array[parent]) {
+					int temp = array[parent];
+					array[parent] = array[child];
+					array[child] = temp;
+				}
+				child = parent;
+			}
+		}
+		return array;
 	}
 }
