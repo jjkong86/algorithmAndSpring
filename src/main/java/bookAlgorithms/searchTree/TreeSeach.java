31936aac1b472ebaf2814134cc0d66a94f51a3b3
정재공 <jeagong86@ibizsoftware.net>
2018-12-20 19:14:57 +0900

    chapter 06 검색 트리, 이진 트리

---
+++ src/main/java/bookAlgorithms/searchTree/TreeSeach.java
@@ -0,0 +1,43 @@
+package bookAlgorithms.searchTree;
+
+import java.util.Random;
+
+public class TreeSeach {
+
+	public static void main(String[] args) {
+		Random gen =  new Random();
+		int[] array = new int[10];
+		int targetIndex = 0;
+		for (int i = 0; i < 10; i++) {
+			array[i] = gen.nextInt(100);
+			targetIndex = targetIndex != 0? targetIndex : gen.nextInt(10);
+		}
+		int[] treeArray = {1,2,3,4,5,6,7,8,9,10};
+		int[] result = treeInsert(treeArray, 0);
+		print(result);
+	}
+	
+	public static int[] treeInsert(int[] treeArray, int left) {
+		int len = treeArray.length-1;
+		int middle = len/2;
+		
+		if (left == treeArray.length-1) return treeArray;
+		
+		int temp = treeArray[middle];
+		treeArray[middle] = treeArray[left];
+		treeArray[left] = temp;
+		
+		treeInsert(treeArray, left++);
+		
+		return treeArray;
+	}
+	
+	public static void print(int[] array) {
+		StringBuilder print = new StringBuilder("정렬 후 : ["+array[0]+", ");
+		for (int j = 1; j < array.length; j++) {
+			print.append(array[j]).append(j == array.length-1 ? "" : ", ");
+		}
+		System.out.println(print+"]");
+	}
+
+}
