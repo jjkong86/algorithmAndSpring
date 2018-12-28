fce19dc82bc8d9839de64d4f78f61ee1f5c0a052
정재공 <jjkong86@gmail.com>
2018-12-28 11:31:41 +0900

    no message

--- src/main/java/reetCodeAlgorithms/Swap_Nodes_in_Pairs/Solution.java
+++ src/main/java/reetCodeAlgorithms/Swap_Nodes_in_Pairs/Solution.java
@@ -16,25 +16,25 @@ public class Solution {
         head.next = newNode; // 3,1
         return second;
     }
-	
+
 	public static void main(String[] args) {
 		Solution s = new Solution();
 		ListNode l11 = new ListNode(1);
 		ListNode l12 = new ListNode(2);
 		ListNode l13 = new ListNode(3);
 		ListNode l14 = new ListNode(4);
-		
+
 		l11.next = l12;
 		l12.next = l13;
 		l13.next = l14;
-		
+
 		ListNode result = s.swapPairs(l11);
-		
+
 		String str = "[";
 		while (result != null) {
 			str += result.val;
 			result = result.next;
-			
+
 		}
 		str+= "]";
 		System.out.println(str);
@@ -45,6 +45,6 @@ class ListNode {
 	int val;
 	ListNode next;
 	ListNode(int x) {
-		this.val = x; 
+		this.val = x;
 	}
 }
