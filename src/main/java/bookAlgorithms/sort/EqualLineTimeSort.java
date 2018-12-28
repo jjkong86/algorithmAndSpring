fbff510e53054ebbb7cf5f310a2901dd538a380e
정재공 <jjkong86@gmail.com>
2018-12-28 11:40:21 +0900

    no message

--- src/main/java/bookAlgorithms/sort/EqualLineTimeSort.java
+++ src/main/java/bookAlgorithms/sort/EqualLineTimeSort.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class EqualLineTimeSort {
 
 	private static int[] wArray;
-	
+
 	public static void main(String[] args) {
 		Random gen =  new Random();
 		int[] array = new int[10];
@@ -22,13 +22,13 @@ public class EqualLineTimeSort {
 //		int[] arrayResult = quickSort(0, wArray.length-1);
 //		print(arrayResult);
 	}
-	
+
 	public static int select(int start, int end, int target) {
 		if (start == end) return wArray[start];
-		
+
 		int q = partition(start, end);
 		System.out.println(q);
-		
+
 		if (q == target) {
 			return wArray[target];
 		} else if (q > target){
@@ -37,7 +37,7 @@ public class EqualLineTimeSort {
 		} else if (q < target) {
 			select(q+1, end, target);
 		}
-		
+
 		return wArray[target];
 	}
 
@@ -50,7 +50,7 @@ public class EqualLineTimeSort {
 		}
 		return wArray;
 	}
-	
+
 	private static int partition(int start, int end) {
 		// // 1구역 // target // 3구역 // 4구역(array 처음)
 		int target = wArray[end]; // 맨 끝의 원소를 기준원소로 함
@@ -68,11 +68,11 @@ public class EqualLineTimeSort {
 		int temp = wArray[firstIndex];
 		wArray[firstIndex] = wArray[thirdIndex];
 		wArray[thirdIndex] = temp;
-		
+
 		print(wArray);
 		return  firstIndex;
 	}
-	
+
 	public static void print(int[] array) {
 		StringBuilder print = new StringBuilder("정렬 후 : ["+array[0]+", ");
 		for (int j = 1; j < array.length; j++) {
