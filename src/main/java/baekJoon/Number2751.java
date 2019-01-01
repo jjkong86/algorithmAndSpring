78bc1bf7e7f704108fcad87f988f034e3b19a1b0
jj <jjkong86@gmail.com>
2019-01-01 15:56:47 +0900

    no message

--- src/main/java/baekJoon/Number2751.java
+++ src/main/java/baekJoon/Number2751.java
@@ -1,55 +1,53 @@
 package baekJoon;
 
-import java.util.Random;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Number2751 {
-	
-private static int[] wList;
-	
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-        int len = sc.nextInt();
-        int[] list = new int[len];
-        Random gen =  new Random();
-        for (int i=0; i<len; i++) {
-            int input = sc.nextInt();
-        	list[i] = input;
-        }
-        wList = list;
-        list = quickSort(0, wList.length-1);
-        
-        for (int i=0; i<len; i++) {
-            System.out.println(list[i]);
-        }
-        
-        sc.close();
-	}
-	private static int[] quickSort(int start, int end) {
-		if (start < end) {
-			int result = partition(start, end);
-			quickSort(start, result-1);
-			quickSort(result+1, end);
+		int len;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        try {
+			len = Integer.parseInt(br.readLine());
+			int[] list = new int[len];
+	        for (int i=0; i<len; i++) {
+	            list[i] = Integer.parseInt(br.readLine());
+	        }
+	        list = heapSort(list, len-1);
+            
+            for (int i = list.length-1; i >= 0 ; i--) {
+			    int temp = list[i];
+			    list[i] = list[0];
+			    list[0] = temp;
+			    heapSort(list, i);
+		    }
+            
+	        StringBuilder sb = new StringBuilder();
+	        for (int i=0; i<len; i++) {
+	        	sb.append(list[i]+"\n");
+	        }
+	        System.out.println(sb);
+		} catch (NumberFormatException | IOException e) {
+			e.printStackTrace();
 		}
-		return wList;
+        
 	}
-
-	private static int partition(int start, int end) {
-		// // 1구역 // target // 3구역 // 4구역(array 처음)
-		int target = wList[end]; // 맨 끝의 원소를 기준원소로 함		
-		int firstIndex = start;
-		int thirdIndex = start;
-		for (thirdIndex = start; thirdIndex < end; thirdIndex++) { // thirdIndex : 3구역의 시작 지점
-			if (target >= wList[thirdIndex]) {
-				int temp = wList[thirdIndex];
-				wList[thirdIndex] = wList[firstIndex];
-				wList[firstIndex] = temp;
-				firstIndex++;
+	private static int[] heapSort(int[] array, int len) {
+		
+		for (int i = 1; i < len; i++) {
+			int child = i;
+			
+			while (child > 0) {
+				int parent = child/2;
+				if (array[child] > array[parent]) {
+					int temp = array[parent];
+					array[parent] = array[child];
+					array[child] = temp;
+				}
+				child = parent;
 			}
 		}
-		int temp = wList[firstIndex];
-		wList[firstIndex] = wList[thirdIndex];
-		wList[thirdIndex] = temp;
-		return  firstIndex;
+		return array;
 	}
 }
