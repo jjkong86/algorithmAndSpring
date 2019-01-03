8f003f8d42339a53b161e575e6a39ea45c9d0cda
jj <jjkong86@gmail.com>
2019-01-04 00:04:13 +0900

    stream과 람다 활용 해보자

--- src/main/java/codewars/FindOutlier/FindOutlier.java
+++ src/main/java/codewars/FindOutlier/FindOutlier.java
@@ -1,24 +1,31 @@
 package codewars.FindOutlier;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class FindOutlier {
-	static int find(int[] integers){
-	    List<Integer> evenList = new ArrayList<>();
-	    List<Integer> oddList = new ArrayList<>();
-	    for (int i=0; i<integers.length; i++) {
-		  int temp = integers[i];
-	      if(temp % 2 == 0) {
-	        evenList.add(integers[i]);
-	      } else {
-	        oddList.add(integers[i]);
-	      }
-	    }
-	    if (evenList.size() == 1) {
-	      return evenList.get(0);
-	    } else {
-	      return oddList.get(0);
-	    }
+	static int find(int[] integers) {
+		List<Integer> evenList = new ArrayList<>();
+		List<Integer> oddList = new ArrayList<>();
+		for (int i = 0; i < integers.length; i++) {
+			int temp = integers[i];
+			if (temp % 2 == 0) {
+				evenList.add(integers[i]);
+			} else {
+				oddList.add(integers[i]);
+			}
+		}
+		if (evenList.size() == 1) {
+			return evenList.get(0);
+		} else {
+			return oddList.get(0);
+		}
+	}
+
+	static int find2(int[] integers) {
+		int[] evenArray = Arrays.stream(integers).filter(i -> i % 2 == 0).toArray();
+		int[] oddArray = Arrays.stream(integers).filter(i -> i % 2 != 0).toArray();
+		return evenArray.length == 1 ? evenArray[0] : oddArray[0];
 	}
 }
--- src/main/java/codewars/FindOutlier/OutlierTest.java
+++ src/main/java/codewars/FindOutlier/OutlierTest.java
@@ -11,8 +11,8 @@ public class OutlierTest {
 	     int[] exampleTest1 = {2,6,8,-10,3};
 	     int[] exampleTest2 = {206847684,1056521,7,17,1901,21104421,7,1,35521,1,7781}; 
 	     int[] exampleTest3 = {Integer.MAX_VALUE, 0, 1};
-	     assertEquals(3, FindOutlier.find(exampleTest1));
-	     assertEquals(206847684, FindOutlier.find(exampleTest2));
-	     assertEquals(0, FindOutlier.find(exampleTest3));
+	     assertEquals(3, FindOutlier.find2(exampleTest1));
+	     assertEquals(206847684, FindOutlier.find2(exampleTest2));
+	     assertEquals(0, FindOutlier.find2(exampleTest3));
 	 }
 }
