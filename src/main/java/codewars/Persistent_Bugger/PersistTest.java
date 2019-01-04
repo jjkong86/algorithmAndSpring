fd73c0b394ee5762313eb9b0b614228e727d3c87
정재공 <jjkong86@gmail.com>
2019-01-04 11:57:14 +0900

    Persistent Bugger 알고리즘 -> 재귀 이용함

--- src/main/java/codewars/Persistent_Bugger/PersistTest.java
+++ src/main/java/codewars/Persistent_Bugger/PersistTest.java
@@ -1,6 +1,7 @@
 package codewars.Persistent_Bugger;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 public class PersistTest {
@@ -14,4 +15,33 @@ public class PersistTest {
 		assertEquals(4, Persist.persistence(999));
 	}
 
+	public static class Persist {
+
+		public static int persistence(long n) {
+			// your code
+			int count = 0;
+			if (n < 10) {
+				return count;
+			}
+			count = persistence(n, count+1);
+
+			System.out.println("count : " + count);
+			return count;
+		}
+
+		public static int persistence(long n, int count) {
+			long temp = 1;
+			while (n > 0) {
+				temp *= n % 10;
+				n = n / 10;
+			}
+
+			if (temp > 9) {
+				count = persistence(temp, count+1);
+			} else {
+				return count;
+			}
+			return count;
+		}
+	}
 }
