85ef4da7b6c88f6cb5215d2a1d04d689efb96686
jj <jjkong86@gmail.com>
2019-01-05 21:49:18 +0900

    no message

---
+++ src/main/java/baekJoon/Number11718.java
@@ -0,0 +1,17 @@
+package baekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Number11718 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String input;
+		while ((input = br.readLine()) != null) {
+			System.out.println(input);
+		}
+		br.close();
+	}
+}
