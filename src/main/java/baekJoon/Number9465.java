9e37298f55f0bd18030c23fc6ad279fbb0b296d3
jj <jjkong86@gmail.com>
2019-01-13 22:36:36 +0900

    Arrays.stream(scoreBundle[j]).max().getAsInt(); Stream.of(scoreBundle[j]).max(Comparator.comparing(Integer::valueOf)).get(); string 비교

--- src/main/java/baekJoon/Number9465.java
+++ src/main/java/baekJoon/Number9465.java
@@ -3,6 +3,7 @@ package baekJoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Optional;
 import java.util.StringJoiner;
@@ -10,7 +11,7 @@ import java.util.stream.Stream;
 
 public class Number9465 {
 
-	public static int maxScore(String[][] str, int cardNum) {
+	public static int maxScore(int[][] str, int cardNum) {
 		//최대값 위주로 때면 될까?
 		
 		
@@ -23,13 +24,16 @@ public class Number9465 {
 		StringJoiner sj = new StringJoiner("\n");
 		for (int i = 0; i < len; i++) {
 			int cardNum = Integer.parseInt(br.readLine());
-			String[][] scoreBundle = new String[2][cardNum];
+			int[][] scoreBundle = new int[2][cardNum];
+			int max = 0;
 			for (int j = 0; j < scoreBundle.length; j++) {
 				String[] split = br.readLine().split(" ");
 				for (int k = 0; k < split.length; k++) {
-					scoreBundle[j][k] = split[k];
+					scoreBundle[j][k] = Integer.parseInt(split[k]);
 				}
-				Optional<String> max = Stream.of(scoreBundle[j]).max(Comparator.comparing(Integer::valueOf));
+				max = Arrays.stream(scoreBundle[j]).max().getAsInt();
+				// Stream.of(scoreBundle[j]).max(Comparator.comparing(Integer::valueOf)).get(); string 비교
+				System.out.println(max);
 			}
 			sj.add(String.valueOf(maxScore(scoreBundle, cardNum)));
 		}
