5a550cdc59d0a2124b4e58a4a320124427aa84a7
정재공 <jjkong86@gmail.com>
2019-01-14 13:19:07 +0900

    백준 9465, dp문제  - index 0 부터 가장 높은 점수를 채워 나가는 방식으로 해결

--- src/main/java/baekJoon/Number9465.java
+++ src/main/java/baekJoon/Number9465.java
@@ -3,19 +3,25 @@ package baekJoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.Optional;
 import java.util.StringJoiner;
-import java.util.stream.Stream;
 
 public class Number9465 {
 
-	public static int maxScore(int[][] str, int cardNum) {
-		//최대값 위주로 때면 될까?
+	public static int maxScore(int[][] arrays) {
+		//arrays[0][n]과 arrays[1][n]이 초기 경우의 수
+		int len = arrays[0].length;
 		
+		int[][] dp = new int[2][len+1];
+		dp[0][0] = dp[1][0] = 0;
+		dp[0][1] = arrays[0][0];
+		dp[1][1] = arrays[1][0];
 		
-		return 0;
+		for (int i = 2; i <= len; i++) {
+			dp[0][i] = Math.max(dp[1][i-1], dp[1][i-2])+arrays[0][i-1];
+			dp[1][i] = Math.max(dp[0][i-1], dp[0][i-2])+arrays[1][i-1];
+		}
+		
+		return Math.max(dp[1][len], dp[0][len]);
 	}
 	
 	public static void main(String[] args) throws NumberFormatException, IOException {
@@ -25,18 +31,15 @@ public class Number9465 {
 		for (int i = 0; i < len; i++) {
 			int cardNum = Integer.parseInt(br.readLine());
 			int[][] scoreBundle = new int[2][cardNum];
-			int max = 0;
 			for (int j = 0; j < scoreBundle.length; j++) {
 				String[] split = br.readLine().split(" ");
 				for (int k = 0; k < split.length; k++) {
 					scoreBundle[j][k] = Integer.parseInt(split[k]);
 				}
-				max = Arrays.stream(scoreBundle[j]).max().getAsInt();
-				// Stream.of(scoreBundle[j]).max(Comparator.comparing(Integer::valueOf)).get(); string 비교
-				System.out.println(max);
 			}
-			sj.add(String.valueOf(maxScore(scoreBundle, cardNum)));
+			sj.add(String.valueOf(maxScore(scoreBundle)));
 		}
+		System.out.println(sj);
 		br.close();
 	}
 }
