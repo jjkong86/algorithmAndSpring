af67c97c075113bc40ac30b9381e1657f62096d5
정재공 <jjkong86@gmail.com>
2019-01-18 13:49:45 +0900

    백준 2579, 계단 오르기

--- src/main/java/baekJoon/Number2579/Main.java
+++ src/main/java/baekJoon/Number2579/Main.java
@@ -3,24 +3,33 @@ package baekJoon.Number2579;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 public class Main {
-	public static int stairClimb(int[] array) {
+	public static int stairClimb(int[] array, int len) {
 		// 계단은 1단계, 2단계 오를 수 있다
 		// 연속된 세 개의 계단은 안됨 -> 2개까지만, 단 시작점 포함 x
 		// 마지막 도착 계단 무조건 밟아야함
-		// ooxo, oxoo, xoox 3가지 경우의 수인듯?
-		
-		
-		return 0;
+		// oxo, xoo, oox 3가지 경우의 수인듯? --> oox는 안됨 -> ex 10 10 1 1 10 계단을 한번에 새개 올라가게됨....
+		int[] dp = new int[(len < 2 ? 2 : len) + 1];
+		dp[0] = 0;
+		dp[1] = array[0];
+		dp[2] = array[0] + array[1];
+		for (int i = 3; i <= len; i++) {
+			int case1 = dp[i - 2] + array[i - 1];
+			int case2 = dp[i - 3] + array[i - 2] + array[i - 1];
+			dp[i] = Math.max(case1, case2);
+		}
+		return dp[len];
 	}
 
 	public static void main(String[] a) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int len = Integer.parseInt(br.readLine());
+		int[] array = new int[len < 2 ? 2 : len];
+		for (int i = 0; i < len; i++) {
+			array[i] = Integer.parseInt(br.readLine());
+		}
+		System.out.println(stairClimb(array, len));
 		br.close();
 	}
-
 }
--- src/main/java/baekJoon/Number2579/README.md
+++ src/main/java/baekJoon/Number2579/README.md
@@ -1,22 +1,27 @@
 ## 문제풀이
- 1. n개의 정수로 이루어진 임의의 수열이 주어진다. 이 중 연속된 몇 개의 수를 선택해서 구할 수 있는 합 중 가장 큰 합
- 2. 연속 되어야 하고, 1개이상
- 3. 하나씩 더해서 1보다 크면 계속 더해감, 0보다 작으면 dp에 들어온 숫자를 넣어준다.
+ 1. 계단 오르기
+ 2. 연속된 세 개의 계단은 안됨 -> 2개까지만, 단 시작점 포함 x
+ 3. 마지막 도착 계단 무조건 밟아야함
+ 4. oxo, xoo, oox 3가지 경우의 수 --> oox는 안됨 -> ex 10 10 1 1 10 계단을 한번에 새개 올라가게됨
+ 
  
 > ```
-> if (dp[i - 1] > 0 ) {
-> 	dp[i] = dp[i - 1] + array[i];
-> } else {
-> 	dp[i] = array[i];
-> }
+> int case1 = dp[i - 2] + array[i - 1];
+> int case2 = dp[i - 3] + array[i - 2] + array[i - 1];
+> dp[i] = Math.max(case1, case2);
 > ```
 
 ## 예제 입력
-10  
-10 -4 3 1 5 6 -35 12 21 -1
+6
+10
+20
+15
+25
+10
+20
 
 ## Link
-https://www.acmicpc.net/problem/1912
+https://www.acmicpc.net/problem/2579
 
 ## MyAccount
 
