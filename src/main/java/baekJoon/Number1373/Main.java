ce09176b32719d9a4c6fc097a02bb6d3400d004f
정재공 <jjkong86@gmail.com>
2019-01-28 09:52:43 +0900

    백준 1373

--- src/main/java/baekJoon/Number1373/Main.java
+++ src/main/java/baekJoon/Number1373/Main.java
@@ -6,31 +6,37 @@ import java.io.InputStreamReader;
 
 public class Main {
     public static String calculator(String str) {
-        //1111 -> (1*1 + 2*1 + 2*2*1)*8square0 + (2*2*2*1)*8square1 
+        //1111 -> (1*1 + 2*1 + 2*2*1)*8square0 + (2*2*2*1)*8square1
+    	//더해가면서 하려고 했지만.. long type 보다 커짐
+    	//3자리씩 끊어서 8보다 작은수로 만들고 string으로 붙어야 할듯
         char[] charArray = str.toCharArray();
         int len = charArray.length;
         StringBuilder sb = new StringBuilder();
-        long base = 8;
-        long target = 0;
+        if (str.equals("0") || str.equals(" ") || str.equals("")) {
+			return "0";
+		}
+        
         long sum = 0;
+        int index = 0;
         for (int i=len-1; i>=0; i--) {
-        	long num = convert(charArray[i], len - i - 1);
-            target = (long) Math.pow(2, len - i - 1);
-            if (target == base) {
-            	sb.append(sum/(base/8));
-                base *= 8;
-                sum = num;
-            } else {
-                sum += num;
-            }
-            if (i == 0) {
-				sb.append(sum/(base/8));
+        	//index%3 == 0 조건으로 하면 될듯
+        	//10001111 -> 10,000,000 + 001,000 + 0,111 ->2 + 1 + 7
+        	sum += convert(charArray[i], index);
+        	if ((index + 1) % 3 == 0) {
+				sb.append(sum);
+				sum = 0;
+				index = 0;
+			} else {
+				index++;
 			}
         }
+        if (sum != 0) {
+			sb.append(sum);
+		}
         return sb.reverse().toString();
     }
-    public static long convert(char c, int index) {
-        return (long) ((c - '0') * Math.pow(2, index));
+    public static int convert(char c, int index) {
+        return (int) ((c - '0') * Math.pow(2, index));
     }
     
     public static void main(String[] args) throws IOException {
--- src/main/java/baekJoon/Number1373/README.md
+++ src/main/java/baekJoon/Number1373/README.md
@@ -1,19 +1,23 @@
 ## 문제풀이
- 1. 진법 변환 2
- 2. 진법에서 나머지가 10 이상이면 char 타입으로 변환 해줘야함
+ 1. 2진수 8진수
+ 2. 2진수를 8진수로 변환은 2진수를 3자리씩 끊어서 8진수로 만들어주면됨
+ 3. 수의 길이가 1,000,000 까지 이므로 8을 넘지 않는 수를 string으로 붙어야줘야함 
+ 4. 예외처리 필요
+ 
  
  
 > ```
->  sb.append((char)(remainder - 10 + 'A'));
+> (int) ((c - '0') * Math.pow(2, index));
+> (index + 1) % 3 == 0
 > ```
 
 ## 예제 입력
 > ```
-> 60466175 36
+> 11001100
 > ```
 
 ## Link
-https://www.acmicpc.net/problem/11005
+https://www.acmicpc.net/problem/1373
 
 ## MyAccount
 
