65e35981a757292cb40b916aa9900cfc417fc865
정재공 <jjkong86@gmail.com>
2019-02-07 10:11:31 +0900

    # WARNING: head commit changed in the meantime
    Statistics for an Athletic Association

--- src/main/java/codewars/Merged_String_Checker/StringMerger.java
+++ src/main/java/codewars/Merged_String_Checker/StringMerger.java
@@ -2,25 +2,28 @@ package codewars.Merged_String_Checker;
 
 public class StringMerger {
 
-    public static boolean isMerge(String s, String part1, String part2) {
-    	
-    	if (s  == null) return false;
-      int len = s.length();
-      int part1Index = 0;
-      int part2Index = 0;
-      int sucessFlag = 0;
-      for (int i=0; i<len; i++) {
-        char c = s.charAt(i);
-        if (part1.length() > part1Index && c == part1.charAt(part1Index)) {
-          part1Index++;
-        } else if (part2.length() > part2Index && c == part2.charAt(part2Index)) {
-          part2Index++;
-        } else {
-          return false;
-        }
-        sucessFlag++;
-      }
-      
-       return sucessFlag == len;
-    }
+	public static boolean isMerge(String s, String part1, String part2) {
+		System.out.println(s + "::" + part1 + "::" + part2);
+		if (s == null || s.equals(""))
+			return false;
+
+		int len = s.length();
+		int part1Index = 0;
+		int part2Index = 0;
+		int sucessFlag = 0;
+		for (int i = 0; i < len; i++) {
+			char c = s.charAt(i);
+			if (part1.length() > part1Index && c == part1.charAt(part1Index)) {
+				part1Index++;
+			} else if (part2.length() > part2Index && c == part2.charAt(part2Index)) {
+				part2Index++;
+			} else {
+				return false;
+			}
+			sucessFlag++;
+		}
+		System.out.println(sucessFlag + "::" + len);
+		System.out.println(sucessFlag == len);
+		return sucessFlag == len;
+	}
 }
