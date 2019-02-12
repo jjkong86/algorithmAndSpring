a19844ba0ad2d4b866b89a744e960a4c5c7e034a
정재공 <jjkong86@gmail.com>
2019-02-12 19:29:18 +0900

    백준 10872

---
+++ src/main/java/baekJoon/Number10872/Main.java
@@ -0,0 +1,27 @@
+package baekJoon.Number10872;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.stream.IntStream;
+
+public class Main {
+
+	public static int factorial(String str) {
+		if (str.equals("0")) {
+			return 1;
+		}
+		int num = Integer.parseInt(str);
+		int result = 1;
+		for (int i = 2; i <= num; i++) {
+				result *= i;
+		}
+		return result;
+	}
+    
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(factorial(br.readLine()));
+		br.close();
+	}
+}
