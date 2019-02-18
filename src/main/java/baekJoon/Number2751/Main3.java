b627004d3c70412ca18627e4cf25c6ef4a0b32e9
정재공 <jjkong86@gmail.com>
2019-02-18 17:53:32 +0900

    백준 2751 : 힙정렬, 퀵정렬, 빠른 퀵정렬

--- src/main/java/baekJoon/Number2751/Main3.java
+++ src/main/java/baekJoon/Number2751/Main3.java
@@ -4,35 +4,37 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main2 {
+public class Main3 {
+	
 	public static void quickSort(int[] array, int left, int right) {
-		if (right > left) {
+		if (left < right) {
 			int middle = partition(array, left, right);
 			quickSort(array, left, middle-1);
 			quickSort(array, middle+1, right);
 		}
 	}
 	
-	public static int partition(int array[], int left, int right) {
-		//기준이 되는 수를 정하여 왼쪽보다 크면 left++, 기준이 되는 수의 오른쪽이 기준수보다 크면 right--
-		int middle = array[(left + right) / 2];
-		while (left < right) {
-			
-			while (array[left] < middle && left < right)	{
-				left++;
-			}
-			while (array[right] > middle && left < right) {
-				right--;
-			}
-			
-			if (left < right) {
-				int temp = array[left];
-				array[left] = array[right];
-				array[right] = temp;
+	public static int partition(int[] array, int left, int right) {
+		// 1 / 2 / 3 / 4 구역
+		// 기준 숫자보다 작으면 1구역, 크면 2구역, 마지막엔 1구역+1 <-> 기준숫자
+		//
+		int standard = array[right];
+		int firstIdx = left;
+		int thirdIdx = left;
+		
+		for (thirdIdx = left; thirdIdx<=right; thirdIdx++) {
+			if (standard > array[thirdIdx]) {
+				int temp = array[firstIdx];
+				array[firstIdx] = array[thirdIdx];
+				array[thirdIdx] = temp;
+				firstIdx ++;
 			}
 		}
 		
-		return left;
+		array[right] = array[firstIdx];
+		array[firstIdx] = standard;
+		
+		return firstIdx;
 	}
 	
 	public static void main(String[] args) throws NumberFormatException, IOException {
