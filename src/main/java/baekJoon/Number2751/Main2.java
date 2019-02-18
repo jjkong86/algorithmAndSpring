1d3da1f60be6d3d802a690b5669c0069ebb3f180
정재공 <jjkong86@gmail.com>
2019-02-18 13:39:08 +0900

    백준 2751

--- src/main/java/baekJoon/Number2751/Main2.java
+++ src/main/java/baekJoon/Number2751/Main2.java
@@ -5,49 +5,47 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main2 {
-	public static void main(String[] args) {
-		int len;
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        try {
-			len = Integer.parseInt(br.readLine());
-			int[] list = new int[len];
-	        for (int i=0; i<len; i++) {
-	            list[i] = Integer.parseInt(br.readLine());
-	        }
-	        list = heapSort(list, len-1);
-            
-            for (int i = list.length-1; i >= 0 ; i--) {
-			    int temp = list[i];
-			    list[i] = list[0];
-			    list[0] = temp;
-			    heapSort(list, i);
-		    }
-            
-	        StringBuilder sb = new StringBuilder();
-	        for (int i=0; i<len; i++) {
-	        	sb.append(list[i]+"\n");
-	        }
-	        System.out.println(sb);
-		} catch (NumberFormatException | IOException e) {
-			e.printStackTrace();
-		}
-        
-	}
-	private static int[] heapSort(int[] array, int len) {
-		
-		for (int i = 1; i < len; i++) {
-			int child = i;
-			
-			while (child > 0) {
-				int parent = child/2;
-				if (array[child] > array[parent]) {
-					int temp = array[parent];
-					array[parent] = array[child];
-					array[child] = temp;
-				}
-				child = parent;
-			}
+	public static int[] quickSort(int[] array, int left, int right) {
+		if (right > left) {
+			int middle = partition(array, left, right);
+			quickSort(array, left, middle-1);
+			quickSort(array, middle+1, right);
 		}
 		return array;
+	}
+	
+	public static int partition(int array[], int left, int right) {
+		//기준이 되는 수를 정하여 왼쪽보다 크면 left++, 기준이 되는 수의 오른쪽이 기준수보다 크면 right--
+		int middle = (left + right)/2;
+		while (array[left] < array[middle] && left < right)	{
+			left++;
+		}
+		while (array[right] > array[middle] && left < right) {
+			right--;
+		}
+		
+		if (left < right) {
+			int temp = array[left];
+			array[left] = array[right];
+			array[right] = temp;
+		}
+		
+		return left;
+	}
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int number = Integer.parseInt(br.readLine());
+		int[] array = new int[number];
+		for (int i = 0; i < number; i++) {
+			array[i] = Integer.parseInt(br.readLine());
+		}
+		StringBuilder sb = new StringBuilder();
+		array = quickSort(array, 0, number);
+		for (int i = 0; i < number; i++) {
+			sb.append(array[i] + "\n");
+		}
+		System.out.println(sb);
+		br.close();
 	}
 }
