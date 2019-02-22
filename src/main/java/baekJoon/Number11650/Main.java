6023d725358e86626bd0bc2d6905383c7eb377e1
정재공 <jjkong86@gmail.com>
2019-02-22 10:09:00 +0900

    백준 11650

--- src/main/java/baekJoon/Number11650/Main.java
+++ src/main/java/baekJoon/Number11650/Main.java
@@ -5,35 +5,42 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.StringTokenizer;
 
 class Main {
     
 	public static String coordinateSort(int[][] array) {
 		//array[n][0] 의 값들로 정렬하고, 값이 같으면 array[n][1]의 값을 비교하여 정렬
         Arrays.sort(array, new Comparator<int[]>() {
-			@Override
 			public int compare(int[] o1, int[] o2) {
-				// TODO Auto-generated method stub
-				return 0;
+				System.out.println(o1[0]+" "+o1[1]+":::"+o2[0]+" "+o2[1]);
+				if (o1[0] == o2[0]) {
+					return o1[1] - o2[1];
+				} else {
+					return o1[0] - o2[0];
+				}
 			}
-        	
 		});
-        
-        return "";
+        return print(array);
     }
+	public static String print(int[][] array) {
+		StringBuilder sb = new StringBuilder();
+		for (int i=0; i< array.length; i++) {
+			sb.append(array[i][0]).append(" ").append(array[i][1]).append("\n");
+		}
+		return sb.toString();
+	}
     
     public static void main(String[] args) throws IOException, NumberFormatException{
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
 	        int number = Integer.parseInt(br.readLine());
-	        int[][] array = new int[5][2];
-	        
+	        int[][] array = new int[number][2];
 	        for (int i=0; i<number; i++) {
-	            String[] split = br.readLine().split(" ");
-	            array[i][0] = Integer.parseInt(split[0]);
-	            array[i][1] = Integer.parseInt(split[1]);
+	        	StringTokenizer st = new StringTokenizer(br.readLine());
+	        	array[i][0] = Integer.parseInt(st.nextToken());
+	        	array[i][1] = Integer.parseInt(st.nextToken());
 	        }
 	        System.out.println(coordinateSort(array));
-//        br.close();
         }
     }
 }
---
+++ src/main/java/baekJoon/Number11650/README.md
@@ -0,0 +1,29 @@
+## 문제풀이
+ 1. 좌표 정렬하기
+ 2. Arrays.sort 메소드를 사용하고, Comparator 인터페이스를 재정의함
+ 
+> ```
+> Arrays.sort(array, new Comparator<int[]>() {
+> 	public int compare(int[] o1, int[] o2) {
+> 	}
+> });
+> ```
+
+## 예제 입력
+5
+0 4
+1 2
+1 -1
+2 2
+3 3
+
+## Link
+https://www.acmicpc.net/problem/11650
+
+## MyAccount
+
+> backjoon : <https://www.acmicpc.net/user/jjkong86>  
+> leetCode : <https://leetcode.com/jjkong86/> 
+
+
+
