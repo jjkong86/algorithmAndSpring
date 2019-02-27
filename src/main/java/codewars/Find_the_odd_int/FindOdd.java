72fdc8b11c47f1ba46013602d6fdda3d4f110ade
정재공 <jjkong86@gmail.com>
2019-02-27 18:17:25 +0900

    xor 연산 생각 !

--- src/main/java/codewars/Find_the_odd_int/FindOdd.java
+++ src/main/java/codewars/Find_the_odd_int/FindOdd.java
@@ -9,6 +9,7 @@ import java.util.stream.IntStream;
 public class FindOdd {
 	public static int findIt(int[] array) {
 		// 주어진 배열에서 홀수번 반복되는 숫자를 찾는 문제인듯
+		// xor 연산을 하면 clever : 홀수번 반복되는 숫자가 오직 하나 이기 때문에 xor 연산을 하면 짝수 숫자들은 모두 0이 되버림.. ex ) xor ^= array[i]
 		Map<Integer, Integer> hMap = new HashMap<>();
 		IntStream.of(array).forEach(i -> hMap.put(i, hMap.get(i) == null ? 1 : hMap.get(i) + 1));
 		List<Integer> list = new ArrayList<>(hMap.keySet());
--- src/main/java/etc/test.java
+++ src/main/java/etc/test.java
@@ -8,30 +8,15 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Stack;
+import java.util.stream.IntStream;
 
 import com.mysql.jdbc.StringUtils;
 
 public class test {
 
 	public static void main(String[] args) throws NumberFormatException, IOException, ParseException {
-		String s = "1 3 +";
-		Stack<Integer> stack = new Stack<>();
-		String[] split = s.split(" ");
-		int len = split.length;
-		for (int i = 0; i < len; i++) {
-			String str = split[i];
-			char c = str.charAt(0);
-			if (c - '0' >= 0 && c - '0' <= 9) {
-				stack.push(Integer.parseInt(str));
-			} else if (c == '+') {
-				int a = stack.pop();
-				int b = stack.pop();
-				stack.push(b + a);
-			}
-		}
-		System.out.println(Double.parseDouble(s));
-		
-		System.out.println((double)stack.pop());
+		int[] array = new int[] {5,654,3,4,5};
+		System.out.println(IntStream.of(array).reduce(0, Integer::sum));
 		
 	}
 
