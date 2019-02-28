2f91630aa7f7e7a2172e355e193f9b73e2901a5d
정재공 <jjkong86@gmail.com>
2019-02-28 11:17:57 +0900

    codewars : Decode the Morse code

--- src/main/java/codewars/Decode_the_Morse_code/MorseCodeDecoder.java
+++ src/main/java/codewars/Decode_the_Morse_code/MorseCodeDecoder.java
@@ -1,18 +1,15 @@
 package codewars.Decode_the_Morse_code;
 
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class MorseCodeDecoder {
     public static String decode(String morseCode) {
         // your brilliant code here, remember that you can access the preloaded Morse code table through MorseCode.get(code)
-        StringBuilder sb = new StringBuilder();
-        String[] word = morseCode.split("   ");
-        for (int i=0; i<word.length; i++) {
-          String[] character = word[i].split(" ");
-          
-          for (int j=0; j<character.length; j++) {
-            sb.append(MorseCode.get(character(j)));
-          }
-          sb.append(" ");
-        }
-        return sb.toString();
+    	return Stream.of(morseCode.trim().split("   ")).map(i -> decodeWord(i)).collect(Collectors.joining(" "));
+    }
+    public static String decodeWord(String str) {
+//    	return Stream.of(str.trim().split(" ")).map(i -> MorseCode.get(i)).collect(Collectors.joining());
+    	return "";
     }
 }
