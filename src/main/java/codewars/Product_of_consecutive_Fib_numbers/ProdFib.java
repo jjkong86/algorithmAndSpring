ee631e7c2a44859459edcb1f102258c0fcca2f84
정재공 <jjkong86@gmail.com>
2019-04-12 13:22:01 +0900

    clever solution

--- src/main/java/codewars/Product_of_consecutive_Fib_numbers/ProdFib.java
+++ src/main/java/codewars/Product_of_consecutive_Fib_numbers/ProdFib.java
@@ -22,6 +22,20 @@ public class ProdFib {
 			}
 		}
 	}
+	
+	public static long[] productFib2(long prod) {
+		// your code
+		// 피보나치 함수를 구하고, F(n) * F(n+1) 되는 숫자면 true를 반환
+		// F(n) * F(n+1) < prod < F(n+1) * F(n+2) 라면 F(n+1), F(n+2), false 반환
+		long a = 0l;
+		long b = 1l;
+		while (a * b < prod) {
+			long temp = a;
+			a = b;
+			b = temp + b; 
+		}
+		return new long[] {a, b, a * b == prod ? 1 : 0};
+	}
 
 	public static long fibo(long[] array, int index) {
 		return array[index] = array[index - 1] + array[index - 2];
--- src/main/java/codewars/Product_of_consecutive_Fib_numbers/ProdFibTest.java
+++ src/main/java/codewars/Product_of_consecutive_Fib_numbers/ProdFibTest.java
@@ -9,12 +9,12 @@ public class ProdFibTest {
 	@Test
 	public void test1() {
 		long[] r = new long[] { 55, 89, 1 };
-		assertArrayEquals(r, ProdFib.productFib(4895));
+		assertArrayEquals(r, ProdFib.productFib2(4895));
 	}
 
 	@Test
 	public void test2() {
 		long[] r = new long[] { 89, 144, 0 };
-		assertArrayEquals(r, ProdFib.productFib(5895));
+		assertArrayEquals(r, ProdFib.productFib2(5895));
 	}
 }
