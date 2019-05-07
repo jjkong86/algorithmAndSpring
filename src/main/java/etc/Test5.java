31d707d21528168a0f84fce797f77359f4582405
정재공 <jjkong86@gmail.com>
2019-05-07 09:59:22 +0900

    no message

--- src/main/java/etc/Test5.java
+++ src/main/java/etc/Test5.java
@@ -12,6 +12,33 @@ public class Test5 {
 		String str = "532";
 		char[] c = str.toCharArray();
 		System.out.println(Integer.parseInt(new String(c)));
+		
+		for (int i = 1; i < 10; i++) {
+			
+		}
 	}
+	
+	public static void primerize(int[][] array, int num) {
+        int cal = num;
+        int twoCount = 0, fiveCount = 0;
+        while (cal > 1) {
+            if (array[cal][0] != 0 || array[cal][1] != 0) { //캐싱을 위해
+                twoCount += array[cal][0];
+                fiveCount += array[cal][1];
+                break;
+            }
+            if (cal % 2 == 0) {
+                cal /= 2;
+                twoCount++;
+            } else if (cal % 5 == 0) {
+                cal /= 5;
+                fiveCount++;
+            } else {
+                break;
+            }
+        }
+        array[num][0] = twoCount;
+        array[num][1] = fiveCount;
+    }
 	
 }
--- src/main/java/reetCodeAlgorithms/Factorial_Trailing_Zeroes/Solution.java
+++ src/main/java/reetCodeAlgorithms/Factorial_Trailing_Zeroes/Solution.java
@@ -6,34 +6,26 @@ class Solution {
 		// 0이 되는 경우의 수 : 2*5 -> 각 주어지는 숫자를 소인수 분해하여
 		// 2와 5애 개수
 		int fiveCount = 0;
-		System.out.println(n/5);
-		int[] array = new int[n/5+1];
 		for (int i = 5; i <= n; i+=5) {
-            fiveCount += primerize(array, i);
+            fiveCount += primerize(i);
 		}
 		return fiveCount;
 	}
 
-	public static int primerize(int[] array, int num) {
-		int cal = num;
+	public static int primerize(int num) {
 		int fiveCount = 0;
-		while (cal > 1) {
-			if (cal % 5 == 0) {
-				cal /= 5;
+		while (num > 1) {
+			if (num % 5 == 0) {
+				num /= 5;
 				fiveCount++;
-				if (array[cal/5] != 0) {
-  					array[num/5] = fiveCount + array[cal/5];
-					return array[num/5];
-				}
 			} else {
 				break;
 			}
 		}
-		array[num/5] = fiveCount;
 		return fiveCount;
 	}
 	
 	public static void main(String[] args) {
-		System.out.println(trailingZeroes(2147483647));
+		System.out.println(trailingZeroes(3213));
 	}
 }
