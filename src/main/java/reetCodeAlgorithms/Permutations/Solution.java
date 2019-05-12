21cefa4268a0fff43dc27cb249fc004387dbb4d1
JEONG JEAGONG <jjkong86@gmail.com>
2019-05-12 22:33:58 +0900

    Create Solution.java

---
+++ src/main/java/reetCodeAlgorithms/Permutations/Solution.java
@@ -0,0 +1,14 @@
+
+package reetCodeAlgorithms.Permutations;
+import java.util.*;
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        int len = nums.length;
+        int index = 0;
+        List<List<Integer>> result = new ArrayList<>();
+        while (true) {
+            List<Integer> list = new ArrayList<>();
+            
+        }
+    }
+}
