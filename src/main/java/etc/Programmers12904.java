86955c7eb25966997fd21a97e89cf6629f46022e
정재공 <jjkong86@gmail.com>
2019-05-21 18:43:00 +0900

    no message

--- src/main/java/etc/Programmers12904.java
+++ src/main/java/etc/Programmers12904.java
@@ -18,7 +18,7 @@ public class Programmers12904 {
 		int answer = 0;
 		char[] chars = s.toCharArray();
 		int len = chars.length;
-		System.out.println("String >> " + s);
+//		System.out.println("String >> " + s);
 		for (int i = 0; i < len; i++) {
 			for (int j = i + 1; j < chars.length; j++) {
 				if (chkPalindrome(chars, i, j)) {
@@ -52,30 +52,30 @@ public class Programmers12904 {
 			assertEquals(3, p.solution("abacde"));
 		}
 
-//		@Test
-//		public void test3() {
-//			assertEquals(7, p.solution("aaaaaaa"));
-//
-//			assertEquals(7, p.solution("abcdcba"));
-//			assertEquals(3, p.solution("abacde"));
-//
-//			assertEquals(7, p.solution("aaaeeddddde"));
-//			assertEquals(0, p.solution("ajsfdkldsfjkl"));
-//			assertEquals(0, p.solution("abcdefg"));
-//			assertEquals(13, p.solution("abcbaedeabcba"));
-//			assertEquals(5, p.solution("adedaa"));
-//			assertEquals(5, p.solution("aadeda"));
-//			assertEquals(4, p.solution("aaaa"));
-//
-//			assertEquals(5, p.solution("ababa"));
-//			assertThat(0, not(p.solution("bbaa")));
-//			assertThat(3, not(p.solution("baaba")));
-//			assertEquals(1, p.solution("a"));
-//			assertThat(0, not(p.solution("aa")));
-//
-//			assertEquals(33, p.solution("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"));
-//			assertEquals(19, p.solution("bbbbaaaaaaaaaaaaaaaaaaaccccceeee"));
-//			assertEquals(21, p.solution("bbbbaaaaaaaaaaaaaaaaaaabcccceeee"));
-//		}
+		@Test
+		public void test3() {
+			assertEquals(7, p.solution("aaaaaaa"));
+
+			assertEquals(7, p.solution("abcdcba"));
+			assertEquals(3, p.solution("abacde"));
+
+			assertEquals(7, p.solution("aaaeeddddde"));
+			assertEquals(0, p.solution("ajsfdkldsfjkl"));
+			assertEquals(0, p.solution("abcdefg"));
+			assertEquals(13, p.solution("abcbaedeabcba"));
+			assertEquals(5, p.solution("adedaa"));
+			assertEquals(5, p.solution("aadeda"));
+			assertEquals(4, p.solution("aaaa"));
+
+			assertEquals(5, p.solution("ababa"));
+			assertThat(0, not(p.solution("bbaa")));
+			assertThat(3, not(p.solution("baaba")));
+			assertEquals(1, p.solution("a"));
+			assertThat(0, not(p.solution("aa")));
+
+			assertEquals(33, p.solution("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"));
+			assertEquals(19, p.solution("bbbbaaaaaaaaaaaaaaaaaaaccccceeee"));
+			assertEquals(21, p.solution("bbbbaaaaaaaaaaaaaaaaaaabcccceeee"));
+		}
 	}
 }
---
+++ src/main/java/reetCodeAlgorithms/Number_of_Longest_Increasing_Subsequence/Solution.java
@@ -0,0 +1,9 @@
+package reetCodeAlgorithms.Number_of_Longest_Increasing_Subsequence;
+
+public class Solution {
+	public int findNumberOfLIS(int[] nums) {
+		
+		
+		return 0;
+    }
+}
