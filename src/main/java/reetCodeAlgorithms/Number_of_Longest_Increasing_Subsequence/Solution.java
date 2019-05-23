7c1f58cea844f43501f8843ae2709391a3cb3fc2
정재공 <jjkong86@gmail.com>
2019-05-23 14:12:35 +0900

    leetCode : Number of Longest Increasing Subsequence(풀고 있음)

--- src/main/java/reetCodeAlgorithms/Number_of_Longest_Increasing_Subsequence/Solution.java
+++ src/main/java/reetCodeAlgorithms/Number_of_Longest_Increasing_Subsequence/Solution.java
@@ -1,9 +1,53 @@
 package reetCodeAlgorithms.Number_of_Longest_Increasing_Subsequence;
 
-public class Solution {
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+class Solution {
 	public int findNumberOfLIS(int[] nums) {
-		
-		
-		return 0;
+        // 정렬되지 않은 정수 배열이 주어지면 가장 길게 증가하는 서브 시퀀스의 수 return
+        // int[][] array = new int[nums.length][2]
+        // 각 index에 증가하는 서브 시퀀스의 길이와 개 수를 저장함
+        if (nums.length < 1) return 0;
+        
+        int[] array = new int[nums.length];
+        array[0] = 1;
+        int max = 1;
+        int maxNum = 0;
+        for (int i=1; i<nums.length; i++) {
+            array[i] = calLength(nums, array, i, i-1, max);
+            if (max < array[i]) {
+                max = array[i];
+                maxNum = nums[i];
+            }
+        }
+        
+        int count = 0;
+        for (int i=0; i<array.length; i++) {
+            if (array[i] == max-1 && nums[i] < maxNum) {
+                count++;
+            }
+        }
+        
+        return count;
     }
+    
+    public int calLength(int[] nums, int[] array, int i, int prev, int max) {
+        if (prev < 0) return 1;
+        if (max > array[prev] || nums[i] < nums[prev]) {
+            return calLength(nums, array, i, prev-1, max);
+        }
+        return array[prev]+1;
+    }
+    
+    public static class TestClass {
+    	Solution s = new Solution();
+    	@Test
+    	public void test1() {
+    		assertThat(2, is(s.findNumberOfLIS(new int[] {1,3,5,4,7})));
+    	}
+    }
+    
 }
