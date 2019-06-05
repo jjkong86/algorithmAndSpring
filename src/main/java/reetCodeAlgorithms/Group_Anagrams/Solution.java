668d7e1048f9d6fc4874a79ec236d97216b23797
정재공 <jjkong86@gmail.com>
2019-06-05 19:04:36 +0900

    set -> linkedList 변경

--- src/main/java/reetCodeAlgorithms/Group_Anagrams/Solution.java
+++ src/main/java/reetCodeAlgorithms/Group_Anagrams/Solution.java
@@ -17,40 +17,45 @@ class Solution {
     		return list;
         }
     	
-        Set<String> set = new HashSet<>(Arrays.asList(strs));
+        List<String> saveList = new LinkedList<>(Arrays.asList(strs));
+        System.out.println(saveList.size());
         List<List<String>> res = new ArrayList<>();
         
         for (int i=0; i<strs.length; i++) {
-            if (!set.contains(strs[i])) continue;
-            List<String> tempList = new ArrayList<>(findAnagrams(new HashSet<String>(), set, strs[i], ""));
+            if (!saveList.contains(strs[i])) continue;
+            List<String> tempList = new ArrayList<>();
+            findAnagrams(tempList, saveList, strs[i], "");
             if (!tempList.isEmpty()) {
             	Collections.sort(tempList);
                 res.add(tempList);
             }
         }
+        
         Collections.sort(res, new Comparator<List<String>>() {
         	@Override
         	public int compare(List<String> o1, List<String> o2) {
         		return Integer.compare(o1.size(), o2.size());
         	}
 		});
+        
         System.out.println(res.toString());
         return res;
     }
     
-    public Set<String> findAnagrams(Set<String> tempSet, Set<String> set, String str, String save) {
-        if (str.length() == 0 || set.size() == 0) {
-        	if (set.contains(save)) {
-        		tempSet.add(save);
-        		set.remove(save);
+    public void findAnagrams(List<String> tempList, List<String> saveList, String str, String save) {
+        if (str.length() == 0 || saveList.size() == 0) {
+        	if (saveList.contains(save)) {
+        		tempList.add(save);
+        		saveList.remove(save);
         	}
-    		return tempSet;
+    		return;
         }
         
         for (int i=0; i<str.length(); i++) {
-            tempSet = findAnagrams(tempSet, set, substractStr(str, i), String.valueOf(str.charAt(i))+save);
+        	findAnagrams(tempList, saveList, substractStr(str, i), String.valueOf(str.charAt(i))+save);
         }
-        return tempSet;
+        
+        return;
     }
     
     public String substractStr(String str, int index) {
@@ -68,12 +73,12 @@ class Solution {
     				is(s.groupAnagrams(new String[] {"eat", "tea", "tan", "ate", "nat", "bat"})));
     	}
     	
-//    	@Test
-//    	public void test2() {
-//    		Solution s = new Solution();
-//    		assertThat("[[,]]",  
-//    				is(s.groupAnagrams(new String[] {"", ""})));
-//    	}
+    	@Test
+    	public void test2() {
+    		Solution s = new Solution();
+    		assertThat("[[],[]]",  
+    				is(s.groupAnagrams(new String[] {"", ""})));
+    	}
     }
     
 }
