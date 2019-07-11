0284c33021db7095f8c805f0524dd5a8123106bf
정재공 <jjkong86@gmail.com>
2019-07-11 19:03:11 +0900

    leetCode : Best Time to Buy and Sell Stock with Cooldown

--- src/main/java/reetCodeAlgorithms/Best_Time_to_Buy/Solution.java
+++ src/main/java/reetCodeAlgorithms/Best_Time_to_Buy/Solution.java
@@ -1,14 +1,30 @@
 package reetCodeAlgorithms.Best_Time_to_Buy;
 
 public class Solution {
-	public int maxProfit(int[] prices) {
-	    int sell = 0, prev_sell = 0, buy = Integer.MIN_VALUE, prev_buy;
-	    for (int price : prices) {
-	        prev_buy = buy;
-	        buy = Math.max(prev_sell - price, prev_buy);
-	        prev_sell = sell;
-	        sell = Math.max(prev_buy + price, prev_sell);
-	    }
-	    return sell;
+    public static int maxProfit(int[] prices) {
+        if (prices == null || prices.length < 1) {
+            return 0;
+        }
+        
+        int length = prices.length;
+        // buy[i]: max profit if the first "i" days end with a "buy" day
+        int[] buy = new int[length + 1];
+        // buy[i]: max profit if the first "i" days end with a "sell" day
+        int[] sell = new int[length + 1];
+        
+        buy[1] = -prices[0];
+        
+        for (int i = 2; i <= length; i++) {
+            int price = prices[i - 1];
+            buy[i] = Math.max(buy[i - 1], sell[i - 2] - price);
+            sell[i] = Math.max(sell[i - 1], buy[i - 1] + price);
+        }
+        
+        // sell[length] >= buy[length]
+        return sell[length];
+    }
+    
+	public static void main(String[] args) {
+		maxProfit(new int[] {1,2,3,4,0,2});
 	}
 }
