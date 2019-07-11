b76db02ab7cb624775d71a4d10e0624e147c4fca
정재공 <jjkong86@gmail.com>
2019-07-11 20:00:18 +0900

    leetCode : Number of Longest Increasing Subsequence

--- src/main/java/reetCodeAlgorithms/Number_of_Longest_Increasing_Subsequence/Solution.java
+++ src/main/java/reetCodeAlgorithms/Number_of_Longest_Increasing_Subsequence/Solution.java
@@ -10,26 +10,40 @@ class Solution {
         // 정렬되지 않은 정수 배열이 주어지면 가장 길게 증가하는 서브 시퀀스의 수 return
         // int[][] array = new int[nums.length][2]
         // 각 index에 증가하는 서브 시퀀스의 길이와 개 수를 저장함
-		int n = nums.length, res = 0, max_len = 0;
-        int[] len =  new int[n], cnt = new int[n];
-        for(int i = 0; i<n; i++){
-            len[i] = cnt[i] = 1;
-            for(int j = 0; j <i ; j++){
-                if(nums[i] > nums[j]){
-                    if(len[i] == len[j] + 1)cnt[i] += cnt[j];
-                    if(len[i] < len[j] + 1){
-                        len[i] = len[j] + 1;
-                        cnt[i] = cnt[j];
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        
+        int len = nums.length;
+        // dp[i] means the lengh of LIS ending at i
+        int[] dp = new int[len];
+        Arrays.fill(dp, 1);
+        // cnt[i] means # LIS ending at i
+        int[] cnt = new int[len];
+        
+        int ret = 0, max = 1;
+        for (int i = 0; i < len; i++) {
+            int count = 1;
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    if (1 + dp[j] > dp[i]) {
+                        count = cnt[j];
+                        dp[i] = 1 + dp[j];
+                    } else if (1 + dp[j] == dp[i]) {
+                        count += cnt[j];
                     }
                 }
             }
-            if(max_len == len[i])res += cnt[i];
-            if(max_len < len[i]){
-                max_len = len[i];
-                res = cnt[i];
+            cnt[i] = count;
+            
+            if (dp[i] > max) {
+                ret = count;
+                max = dp[i];
+            } else if (dp[i] == max) {
+                ret += count;
             }
         }
-        return res;
+        return ret;
     }
     
     public static class TestClass {
@@ -40,5 +54,4 @@ class Solution {
 //    		assertThat(5, is(s.findNumberOfLIS(new int[] {2,2,2,2,2})));
     	}
     }
-    
 }
