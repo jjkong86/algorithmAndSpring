92fdaa6bbc4b704213b692b1326087e6a866b4c8
정재공 <jjkong86@gmail.com>
2019-08-07 19:14:06 +0900

    leetCode : Maximum Product Subarray, 푸는 중

---
+++ src/main/java/etc/Test9.java
@@ -0,0 +1,40 @@
+package etc;
+
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.util.Arrays;
+import java.util.Random;
+
+public class Test9 {
+	
+	public static class Number {
+		double num;
+		
+		Number(double num) {
+			this.num = num;
+		}
+	}
+	
+	public static void main(String[] args) {
+		double balance = 0.04089218;
+		double totalAmount = 0.00761700;
+		double calFee = 0.00000082;
+		
+		BigDecimal convertBalance =  new BigDecimal(String.valueOf(balance));
+		BigDecimal convertTotalAmount =  new BigDecimal(String.valueOf(totalAmount));
+		BigDecimal totalSumAmount = new BigDecimal("0.007617");
+		
+		if (convertTotalAmount.equals(totalSumAmount) || 
+				convertBalance.compareTo(totalSumAmount.add(new BigDecimal(String.valueOf(calFee)))) > -1) { // 수수료 + 보낼 금액은 잔액보다 작거나 같아야함
+			System.out.println("들어옴?");
+		}
+		
+		Number[] nums = new Number[10];
+		for (int i = 0; i < 10; i++) {
+			nums[i] = new Number((double)i);
+		}
+		DecimalFormat fmt = new DecimalFormat("#.##########");
+		BigDecimal sum = Arrays.stream(nums).map(Number -> new BigDecimal(fmt.format(Number.num))).reduce(BigDecimal::add).get();
+		System.out.println(sum);
+	}
+}
---
+++ src/main/java/reetCodeAlgorithms/Solution.java
@@ -0,0 +1,30 @@
+package reetCodeAlgorithms;
+
+import java.util.*;
+class Solution {
+    public int maxProduct(int[] nums) {
+        // 연속된 하위 배열에서 곱이 가장 큰 수
+        // -도 있기 때문에 -값도 가지고 다녀야할듯??
+        // -3, -2, -1, 0, 1, 2
+        if (nums.length == 0) return 0;
+        
+        int len = nums.length;
+        int[] maxArray = new int[len];
+        int[] minArray = new int[len];
+        maxArray[0] = nums[0];
+        minArray[0] = nums[0];
+        int max = nums[0];
+        for (int i=1; i<len; i++) {
+            if (nums[i-0] <= nums[i]) {
+                maxArray[i] = Math.max(maxArray[i-1]*nums[i],nums[i]);
+                
+            } else {
+                maxArray[i] = nums[i];
+            }
+            System.out.println(Arrays.toString(maxArray));
+            //System.out.println(Arrays.toString(minArray));
+            if (maxArray[i] > max) max = maxArray[i];
+        }
+        return max;
+    }
+}
