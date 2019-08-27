875280b7011fb5e758fa1643f5dcaf456bb9c36f
정재공 <jjkong86@gmail.com>
2019-08-27 20:43:07 +0900

    Permutations II -> 최적화

--- src/main/java/reetCodeAlgorithms/PermutationsII/Solution2.java
+++ src/main/java/reetCodeAlgorithms/PermutationsII/Solution2.java
@@ -5,54 +5,37 @@ import static org.junit.Assert.assertThat;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 import org.junit.Test;
 
 class Solution2 {
 	public List<List<Integer>> permuteUnique(int[] nums) {
-		if (nums == null || nums.length < 1) return new ArrayList<>();
-		StringBuilder sb = new StringBuilder();
-		IntStream.of(nums).forEach(i -> sb.append(i));
-		
-		Set<List<Integer>> set = new HashSet<>();
-		permutation(set, sb.toString(), new ArrayList<>());
-		List<List<Integer>> list = new ArrayList<>(set);
-		
-        Collections.sort(list, new Comparator<List<Integer>>() {
-			@Override
-			public int compare(List<Integer> o1, List<Integer> o2) {
-				for (int i = 0; i < o1.size(); i++) {
-					if (o1.get(i) == o2.get(i)) continue;
-					else return Integer.compare(o1.get(i), o2.get(i));
-				}
-				return 0;
-			}
-		});
-		
-        System.out.println(list.toString());
-		return list;
-	}
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        if(nums==null || nums.length==0) return res;
+        boolean[] used = new boolean[nums.length];
+        List<Integer> list = new ArrayList<Integer>();
+        Arrays.sort(nums);
+        dfs(nums, used, list, res);
+        System.out.println(res.toString());
+        return res;
+    }
 
-	public void permutation(Set<List<Integer>> set, String str, List<Integer> save) {
-		if (str.isEmpty()) {
-			set.add(save);
+    public void dfs(int[] nums, boolean[] used, List<Integer> list, List<List<Integer>> res){
+        if(list.size()==nums.length){
+            res.add(new ArrayList<Integer>(list));
             return;
         }
-		
-		for (int i = 0; i < str.length(); i++) {
-			List<Integer> convertsava = save.stream().collect(Collectors.toList());
-			convertsava.add(Character.getNumericValue(str.charAt(i)));
-			str = str.substring(i+1);
-			permutation(set, str, save);
-		}
-	}
+        for(int i=0;i<nums.length;i++){
+            if(used[i]) continue;
+            if(i>0 &&nums[i-1]==nums[i] && !used[i-1]) continue;
+            used[i]=true;
+            list.add(nums[i]);
+            dfs(nums,used,list,res);
+            used[i]=false;
+            list.remove(list.size()-1);
+        }
+    }
 	
 	public static class TestClass {
 		Solution2 s = new Solution2();
