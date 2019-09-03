50ba8bd46967662c22d3e85cf8767c49dc9f2dc1
정재공 <jjkong86@gmail.com>
2019-09-03 23:34:12 +0900

    leetCode :  Uncommon Words from Two Sentences

--- src/main/java/reetCodeAlgorithms/Uncommon_Words_from_Two_Sentences/Solution.java
+++ src/main/java/reetCodeAlgorithms/Uncommon_Words_from_Two_Sentences/Solution.java
@@ -3,7 +3,11 @@ package reetCodeAlgorithms.Uncommon_Words_from_Two_Sentences;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import org.junit.Test;
 
@@ -11,55 +15,47 @@ class Solution {
 	public String[] uncommonFromSentences(String A, String B) {
 		// 두 문자열에서 중복 되지 않은 문자열은 반환
 		// 한 문자열 안에서도 중복이 발생 할 수 있음
-		Set<String> setA = makeSet(A.split(" "));
-		Set<String> setB = makeSet(B.split(" "));
-		List<String> res = new ArrayList<>();
-
-		for (String str : setA) {
-			if (!setB.contains(str)) {
-				res.add(str);
-			}
-		}
+		String[] arrayA = A.split(" ");
+		String[] arrayB = B.split(" ");
 		
-		for (String str : setB) {
-			if (!setA.contains(str)) {
-				res.add(str);
-			}
-		}
-
-		String[] result = new String[res.size()];
-		res.toArray(result);
-		System.out.println(Arrays.toString(result));
-		return result;
+		Map<String, Integer> map = new HashMap<>();
+		makeMap(map, arrayA);
+		makeMap(map, arrayB);
+		
+		List<String> list = makeList(map);
+		String[] res = new String[list.size()];
+		list.toArray(res);
+		System.out.println(list.toString());
+		return res;
 	}
-
-	public Set<String> makeSet(String[] array) {
-		Set<String> set = new HashSet<>();
-		Set<String> duple = new HashSet<>();
+	
+	public void makeMap(Map<String, Integer> map, String[] array) {
 		for (String str : array) {
-			if (set.contains(str)) {
-				duple.add(str);
-				continue;
+			map.put(str, map.getOrDefault(str, 0)+1);
+		}
+	}
+	
+	public List<String> makeList(Map<String, Integer> map) {
+		List<String> list = new ArrayList<>();
+		for (Entry<String, Integer> entry : map.entrySet()) {
+			if (entry.getValue() < 2) {
+				list.add(entry.getKey());
 			}
-			set.add(str);
 		}
-		for (String str : duple) {
-			set.remove(str);
-		}
-		return set;
+		return list;
 	}
 
 	public static class TestClass {
 		Solution s = new Solution();
-//		@Test
-//		public void test1() {
-//			assertThat(new String[] {"sweet","sour"}, is(s.uncommonFromSentences("this apple is sweet", "this apple is sour")));
-//		}
-//		
-//		@Test
-//		public void test2() {
-//			assertThat(new String[] {"banana"}, is(s.uncommonFromSentences("apple apple", "banana")));
-//		}
+		@Test
+		public void test1() {
+			assertThat(new String[] {"sweet","sour"}, is(s.uncommonFromSentences("this apple is sweet", "this apple is sour")));
+		}
+		
+		@Test
+		public void test2() {
+			assertThat(new String[] {"banana"}, is(s.uncommonFromSentences("apple apple", "banana")));
+		}
 		
 		@Test
 		public void test3() {
