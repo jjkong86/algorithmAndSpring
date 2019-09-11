9f5c1f02fb4f4c942373421e95d5d944d9baa9e4
정재공 <jjkong86@gmail.com>
2019-09-11 13:41:00 +0900

    leetCode : Top K Frequent Words 1. 반복되는 문자열을 map에 넣어두고 value를 역순으로 정렬함 2. value가 같으면 key를 기준으로 다시 정렬함

--- src/main/java/reetCodeAlgorithms/Top_K_Frequent_Words/Solution.java
+++ src/main/java/reetCodeAlgorithms/Top_K_Frequent_Words/Solution.java
@@ -3,70 +3,57 @@ package reetCodeAlgorithms.Top_K_Frequent_Words;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
 import org.junit.Test;
 
 public class Solution {
-	boolean isDuple = false;
 	public List<String> topKFrequent(String[] words, int k) {
-		if (words.length < 1) return new ArrayList<>(); 
-		
+		if (words.length < 1)
+			return Collections.emptyList();
+
 		Map<String, Integer> map = makeMap(words, k);
-		List<String> list = new ArrayList<>();
-		map.entrySet().stream().sorted(Collections.reverseOrder(Map.Entry.comparingByValue())).forEachOrdered(x -> list.add(x.getKey()));
-		if (isDuple) {
-			Collections.reverse(list);
-			System.out.println(list.toString());
-			return list;
-		}
+		List<String> list = new LinkedList<>();
+		map.entrySet().stream().sorted(sortByValueAndKey()).limit(k).forEachOrdered(x -> list.add(x.getKey()));
 		System.out.println(list.toString());
 		return list;
 	}
 
 	private Map<String, Integer> makeMap(String[] words, int k) {
-		Map<String, Integer> duple = new HashMap<>();
 		Map<String, Integer> map = new HashMap<>();
-		int count = 0;
 		for (String key : words) {
-			map.put(key, map.getOrDefault(key, 0)+1);
-			if (map.get(key) == k) {
-				count ++;
-				duple.put(key, k);
-			}
-		}
-		if (count > 1) {
-			isDuple = true;
-			return duple;
+			map.put(key, map.getOrDefault(key, 0) + 1);
 		}
 		return map;
 	}
-	
+
+	private Comparator<Map.Entry<String, Integer>> sortByValueAndKey() {
+		return (Map.Entry<String, Integer> a, Map.Entry<String, Integer> b) -> a.getValue() == b.getValue()
+				? a.getKey().compareTo(b.getKey())
+				: b.getValue().compareTo(a.getValue());
+	}
+
 	public static class TestClass {
 		@Test
 		public void test1() {
 			Solution s = new Solution();
-			assertThat(Arrays.asList(new String[] {"i", "love"}), 
-					is(s.topKFrequent(new String[] {"i", "love", "leetcode", "i", "love", "coding"}, 2)));
+			assertThat(Arrays.asList(new String[] { "i", "love" }),
+					is(s.topKFrequent(new String[] { "i", "love", "leetcode", "i", "love", "coding" }, 2)));
 		}
-		
+
 		@Test
 		public void test2() {
 			Solution s = new Solution();
-			assertThat(Arrays.asList(new String[] {"the", "is", "sunny", "day"}), 
-					is(s.topKFrequent(new String[] {"the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is"}, 4)));
-		}
-		
-		@Test
-		public void test3() {
-			Solution s = new Solution();
-			assertThat(Arrays.asList(new String[] {"i"}), 
-					is(s.topKFrequent(new String[] {"i", "love", "leetcode", "i", "love", "coding"}, 1)));
+			assertThat(Arrays.asList(new String[] { "the", "is", "sunny", "day" }),
+					is(s.topKFrequent(
+							new String[] { "the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is" },
+							4)));
 		}
 	}
 }
