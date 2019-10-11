0b79e583181b012c97916448db97fa936fbb61ab
정재공 <jjkong86@gmail.com>
2019-10-11 09:51:07 +0900

    시간 복잡도 개선

--- src/main/java/test/배민/Test2.java
+++ src/main/java/test/배민/Test2.java
@@ -47,13 +47,52 @@ class Test2 {
 		return "other";
 	}
 
+	// 시간 복잡도 개선
+	public String solution2(String S) {
+		String[] keys = new String[] { "music", "images", "movies", "other" };
+		Map<String, Integer> map = makeMap();
+
+		String[] split = S.split("\n");
+		Map<String, Integer> result = new HashMap<>();
+		for (String str : split) {
+			String[] inner = str.split(" ");
+			String[] nameAndExtension = inner[0].split("\\."); // 정규식 예약어 이기 때문
+			String extention = "." + nameAndExtension[nameAndExtension.length - 1];
+			String key = map.get(extention) == null ? "other" : keys[map.get(extention)];
+			int space = Integer.parseInt(inner[1].replace("b", ""));
+
+			result.put(key, result.getOrDefault(key, 0) + space);
+		}
+
+		StringJoiner sj = new StringJoiner("\n");
+		for (String str : keys) {
+			StringBuilder sb = new StringBuilder();
+			sb.append(str + " ");
+			sb.append(result.getOrDefault(str, 0) + "b");
+			sj.add(sb);
+		}
+		return sj.toString();
+	}
+
+	private Map<String, Integer> makeMap() {
+		Map<String, Integer> map = new HashMap<>();
+		String[][] values = new String[][] { { ".mp3", ".aac", ".flac" }, { ".jpg", ".bmp", ".gif" },
+				{ ".mp4", ".avi", ".mkv" } };
+		for (int i = 0; i < values.length; i++) {
+			for (String str : values[i]) {
+				map.put(str, i);
+			}
+		}
+		return map;
+	}
+
 	public static class TestClass {
 		Test2 t = new Test2();
 
 		@Test
 		public void test1() {
 
-			assertThat("music 1011b\n" + "images 0b\n" + "movies 10200b\n" + "other 105b", is(t.solution(
+			assertThat("music 1011b\n" + "images 0b\n" + "movies 10200b\n" + "other 105b", is(t.solution2(
 					"my.song.mp3 11b\ngreatSong.flac 1000b\nnot3.txt 5b\nvideo.mp4 200b\ngame.exe 100b\nmov!e.mkv 10000b")));
 
 		}
