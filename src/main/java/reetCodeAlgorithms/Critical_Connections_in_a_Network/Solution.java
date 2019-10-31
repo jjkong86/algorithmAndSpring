33b3ce27536248ab4278d79b7d0b1970e3014b1d
정재공 <jjkong86@gmail.com>
2019-10-31 16:43:09 +0900

    commit

--- src/main/java/reetCodeAlgorithms/Critical_Connections_in_a_Network/Solution.java
+++ src/main/java/reetCodeAlgorithms/Critical_Connections_in_a_Network/Solution.java
@@ -1,11 +1,17 @@
 package reetCodeAlgorithms.Critical_Connections_in_a_Network;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-class Solution {
+import org.junit.Test;
+
+public class Solution {
 	public List<List<Integer>> criticalConnections(int n, List<List<Integer>> connections) {
 		Map<Integer, List<Integer>> map = new HashMap<>();
 		for (List<Integer> list : connections) {
@@ -31,5 +37,22 @@ class Solution {
 			}
 		}
 		return new ArrayList<>();
+	}
+
+	public static class TestClass {
+		Solution s = new Solution();
+
+		@Test
+		public void test1() {
+			Integer[][] array = new Integer[][] { { 0, 1 }, { 1, 2 }, { 2, 0 }, { 1, 3 }, { 3, 4 }, { 4, 5 }, { 5, 3 } };
+			List<List<Integer>> list = new ArrayList<>();
+			for (Integer[] is : array) {
+				List<Integer> temp = new ArrayList<>(Arrays.asList(is));
+				list.add(temp);
+			}
+			
+			assertThat(new ArrayList<>(Arrays.asList(new Integer[] {1, 3})), is(s.criticalConnections(6, list)));
+		}
+
 	}
 }
