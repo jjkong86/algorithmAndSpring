78272524d8f400d34bcf330fbdd4d15a32ac035e
정재공 <jjkong86@gmail.com>
2019-11-20 22:22:29 +0900

    leetCode : Buddy Strings

--- src/main/java/reetCodeAlgorithms/Buddy_Strings/Solution.java
+++ src/main/java/reetCodeAlgorithms/Buddy_Strings/Solution.java
@@ -3,46 +3,54 @@ package reetCodeAlgorithms.Buddy_Strings;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import org.junit.Test;
 
 class Solution {
-	public boolean buddyStrings(String a, String b) {
-		int aLen = a.length(), bLen = b.length();
-		if (aLen != bLen) return false;
-		
-		StringBuilder notEq = new StringBuilder();
-		String tempB = b;
-		for (int i = 0; i < aLen; i++) {
-			if (a.charAt(i) != b.charAt(i)) {
-				notEq.append(String.valueOf(a.charAt(i)));
-			} else {
-				tempB = tempB.replaceFirst(String.valueOf(a.charAt(i)), "");
-			}
+	public boolean buddyStrings(String A, String B) {
+		if (A.length() != B.length())
+			return false;
+		if (A.equals(B)) {
+			Set<Character> s = new HashSet<Character>();
+			for (char c : A.toCharArray())
+				s.add(c);
+			return s.size() < A.length();
 		}
-		return notEq.reverse().toString().equals(tempB);
+		List<Integer> dif = new ArrayList<>();
+		for (int i = 0; i < A.length(); ++i)
+			if (A.charAt(i) != B.charAt(i))
+				dif.add(i);
+		
+		return dif.size() == 2 && A.charAt(dif.get(0)) == B.charAt(dif.get(1))
+				&& A.charAt(dif.get(1)) == B.charAt(dif.get(0));
 	}
-	
+
 	public static class TestClass {
 		Solution s = new Solution();
+
 		@Test
 		public void test1() {
 			assertThat(true, is(s.buddyStrings("ab", "ba")));
 		}
-		
+
 		@Test
 		public void test2() {
-			assertThat(true, is(s.buddyStrings("ab", "ab")));
+			assertThat(false, is(s.buddyStrings("ab", "ab")));
 		}
-		
+
 		@Test
 		public void test3() {
 			assertThat(true, is(s.buddyStrings("aaaaaaabc", "aaaaaaacb")));
 		}
-		
+
 		@Test
 		public void test4() {
 			assertThat(true, is(s.buddyStrings("aaaaaaabcaa", "aaaaaaacbaa")));
 		}
-		
+
 	}
 }
