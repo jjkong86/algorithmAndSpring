e2315211b34d9152270905deff5ba44554557527
정재공 <jjkong86@gmail.com>
2019-12-02 20:12:25 +0900

    leetCode : Group Anagrams

--- src/main/java/reetCodeAlgorithms/Group_Anagrams/Solution2.java
+++ src/main/java/reetCodeAlgorithms/Group_Anagrams/Solution2.java
@@ -6,70 +6,45 @@ import static org.junit.Assert.assertThat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 
 import org.junit.Test;
+
 public class Solution2 {
-	
-	
+
 	public List<List<String>> groupAnagrams(String[] strs) {
-		//각 항목을 정렬하여 찾아보자
-        
-        Map<String, List<String>> map = new LinkedHashMap<>();
-        Map<String, Integer> sortBySize = new HashMap<>();
-        for (String str : strs) {
-            char[] chars = str.toCharArray();
-            Arrays.sort(chars);
-            String sort = String.valueOf(chars);
-            if (!map.containsKey(sort)) {
-                map.put(sort, new ArrayList<String>());
-                sortBySize.put(sort, 0);
-            }
-            map.get(sort).add(str);
-            sortBySize.put(sort, sortBySize.get(sort)+1);
-        }
-        
-        List<Entry<String, Integer>> list = new ArrayList<>(sortBySize.entrySet());
-        list.sort(Entry.comparingByValue()); // value size에 따라 정렬
-        
-        Map<String, List<String>> sorMap = sortByKeyAndValues(map, list);
-        List<List<String>> res = new ArrayList<>(sorMap.values());
-        System.out.println(res.toString());
-		return res;
-    }
-	
-	private Map<String, List<String>> sortByKeyAndValues(Map<String, List<String>> map, List<Entry<String, Integer>> list) {
-		Map<String, List<String>> sortMap = new LinkedHashMap<>();
-		
-		for (int i=0; i<list.size(); i++) {
-			List<String> tempList = map.get(list.get(i).getKey());
-			Collections.sort(tempList);
-			sortMap.put(list.get(i).getKey(), tempList);
+		Map<String, List<String>> map = new LinkedHashMap<>();
+		for (String str : strs) {
+			char[] chars = str.toCharArray();
+			Arrays.sort(chars);
+			String sort = String.valueOf(chars);
+			if (!map.containsKey(sort)) {
+				List<String> list = new ArrayList<>();
+				list.add(str);
+				map.put(sort, list);
+			} else {
+				map.get(sort).add(str);
+			}
 		}
-		return sortMap;
+
+		List<List<String>> list = new LinkedList<>();
+		for (Map.Entry<String, List<String>> tempList : map.entrySet()) {
+			List<String> temp = tempList.getValue();
+			Collections.sort(temp);
+			list.add(0, temp);
+		}
+		return list;
 	}
-	
-	
-	
-public static class TestClass {
-    	
-    	@Test
-    	public void test1() {
-    		Solution2 s = new Solution2();
-    		assertThat("[[ate, tea, eat], [nat, tan], [bat]]",  
-    				is(s.groupAnagrams(new String[] {"eat", "tea", "tan", "ate", "nat", "bat"})));
-    	}
-    	
-    	@Test
-    	public void test2() {
-    		Solution s = new Solution();
-    		assertThat("[[],[]]",  
-    				is(s.groupAnagrams(new String[] {"", ""})));
-    	}
-    }
+
+	public static class TestClass {
+
+		@Test
+		public void test1() {
+			Solution2 s = new Solution2();
+			assertThat("", is(s.groupAnagrams(new String[] { "eat", "tea", "tan", "ate", "nat", "bat" })));
+		}
+	}
 }
