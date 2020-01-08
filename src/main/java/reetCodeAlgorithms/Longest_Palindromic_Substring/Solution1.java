cfd0ab7c26ffff2b7cb343f33a9983588522c739
정재공 <jjkong86@gmail.com>
2020-01-08 21:40:41 +0900

    leetCode : Longest Palindromic Substring 최소 시간 복잡도 > o(n*n), 시간 복잡도 줄일 방법을 생각해봐야 할듯

--- src/main/java/reetCodeAlgorithms/Longest_Palindromic_Substring/Solution1.java
+++ src/main/java/reetCodeAlgorithms/Longest_Palindromic_Substring/Solution1.java
@@ -1,39 +1,61 @@
 package reetCodeAlgorithms.Longest_Palindromic_Substring;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
 class Solution1 {
-    char[] chars;
-    public String longestPalindrome(String s) {
-        int max = 0;
-        String result = "";
-        chars = s.toCharArray();
-        int len = chars.length;
-        
-        for (int i=0; i<len; i++) {
-            for (int j = i+1; j<len; j++) {
-                if (j - i + 1 > max && isPalindrome(i, j)) {
-                    max = Math.max(max, (j - i + 1));
-                    result = makeString(i, j);
-                }
-            }
-        }
-        return result;
-    }
-    
-    private boolean isPalindrome(int left, int right) {
-        System.out.println(makeString(left, right));
-        int end = right;
-		for (int i = left; i <= right; i++) {
-			if (chars[i] != chars[right-i])
-                return false;
+	char[] chars;
+
+	public String longestPalindrome(String s) {
+		if (s == null || "".equals(s))
+			return "";
+		int max = 0;
+		chars = s.toCharArray();
+		int len = chars.length;
+		int maxLeft = 0, maxRight = 0;
+
+		for (int i = 0; i < len; i++) {
+			for (int j = i + 1; j < len; j++) {
+				if (j - i + 1 > max && isPalindrome(i, j)) {
+					max = Math.max(max, (j - i + 1));
+					maxLeft = i;
+					maxRight = j;
+				}
+			}
+		}
+		return makeString(maxLeft, maxRight);
+	}
+
+	private boolean isPalindrome(int left, int right) {
+		int len = (right - left) / 2;
+		for (int i = 0; i <= len; i++) {
+			if (chars[left + i] != chars[right - i])
+				return false;
 		}
 		return true;
-    }
-    
-    private String makeString(int left, int right) {
-        StringBuilder sb = new StringBuilder();
-        for (int i=left; i<=right; i++) {
-            sb.append(String.valueOf(chars[i]));
-        }
-        return sb.toString();
-    }
+	}
+
+	private String makeString(int left, int right) {
+		StringBuilder sb = new StringBuilder();
+		for (int i = left; i <= right; i++) {
+			sb.append(String.valueOf(chars[i]));
+		}
+		return sb.toString();
+	}
+
+	public static class ClassTest {
+		Solution1 s = new Solution1();
+
+		@Test
+		public void test1() {
+			assertThat("bab", is(s.longestPalindrome("babad")));
+		}
+
+		@Test
+		public void test2() {
+			assertThat("bb", is(s.longestPalindrome("cbbd")));
+		}
+	}
 }
