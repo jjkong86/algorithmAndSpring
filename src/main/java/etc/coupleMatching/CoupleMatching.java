126f82eb3d43612fd59d69a9e5b02caf214fabfa
정재공 <jjkong86@gmail.com>
2020-01-19 19:03:16 +0900

    문자열 배열에서 일치율이 가장 높은(중복 가능) 문자의 인덱스를 구성하는 문제

--- src/main/java/etc/coupleMatching/CoupleMatching.java
+++ src/main/java/etc/coupleMatching/CoupleMatching.java
@@ -1,8 +1,9 @@
 package etc.coupleMatching;
 
 import java.io.BufferedReader;
-import java.io.File;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -10,200 +11,128 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.StringJoiner;
 
-// 문자열 2차 배열이 주어지고, 일치율이 가장 높거나 같은 문자열의 인덱스를 출력
-// [abcd], [bcae], [aecg], [bcdt] -> [1,2], [1,4] 
 public class CoupleMatching {
-	static String path = CoupleMatching.class.getResource("").getPath();
-	static final String NAME_10 = path + "10.txt";
-	static final String NAME_100 = path + "100.txt";
-	static final String NAME_10000 = path + "10000.txt";
-	static final String NAME_500000 = path + "500000.txt";
+	String[] strs = null;
+	boolean isMax = false;
+	Map<String, List<Integer>> map = new HashMap<>();
 
-	public static String build1() {
-		// StringJoiner sj = new StringJoiner("\n");
-		List<String> result = new ArrayList<>();
-		String[] strs = null;
-		try (BufferedReader br = new BufferedReader(new FileReader(NAME_10000))) {
-			int peopleCount = Integer.parseInt(br.readLine());
-			strs = new String[peopleCount];
-			int index = 0;
-			while (br.ready()) {
-				char[] temp = br.readLine().replace(" ", "").toCharArray();
-				Arrays.sort(temp);
-				String key = String.valueOf(temp);
-				strs[index++] = key;
-			}
+	public String build(String fileName) {
+		fileRead(fileName); // 파일을 읽어서 데이터를 담음
 
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		if (isMax)
+			return findToMap(map); // 문자열이 일치하여 map에서 값을 가져옴
 
-		int max = 0;
-		for (int i = 0; i < strs.length; i++) {
-			char[] chars = strs[i].toCharArray();
-			for (int j = i + 1; j < strs.length; j++) {
-				char[] tempChars = strs[j].toCharArray();
-				int count = 0;
-				for (int t = 0; t < chars.length; t++) {
-					if (chars[t] == tempChars[t]) {
-						count++;
-					}
-
-					if (10 - t + (count - 1) < max) {
-						break;
-					}
-				}
-
-				if (count >= max) {
-					if (count > max) {
-						result.clear();
-						max = count;
-					}
-					StringBuilder sb = new StringBuilder();
-					sb.append(String.valueOf(i + 1)).append("-").append(String.valueOf(j + 1));
-					result.add(sb.toString());
-				}
-			}
-		}
-
-		return String.valueOf(result.size());
+		return findToArray();
 	}
 
-	public static String build2() {
-		// StringJoiner sj = new StringJoiner("\n");
-		List<String> result = new ArrayList<>();
-		String[] strs = null;
-		boolean isMax = false;
-		Map<String, List<Integer>> map = new HashMap<>();
-		try (BufferedReader br = new BufferedReader(new FileReader(NAME_500000))) {
+	private void fileRead(String fileName) {
+		try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
 			int peopleCount = Integer.parseInt(br.readLine());
 			strs = new String[peopleCount];
 			int index = 0;
 			while (br.ready()) {
-				char[] temp = br.readLine().replace(" ", "").toCharArray();
-				Arrays.sort(temp);
-				String key = String.valueOf(temp);
-				if (map.get(key) == null) {
-					map.put(key, new ArrayList<>());
-				}
-				List<Integer> values = map.get(key);
-				values.add(index);
-				if (!isMax && values.size() > 1) {
+				String key = sortedString(br.readLine());
+				int valueCount = buildMap(map, key, index + 2); // 문자열이 일치 하는 경우 사용하기 위해 map에 담아둠
+
+				if (!isMax && valueCount > 1) { // 문자열이 일치 하여 map으로 사용하면 되기 때문에 배열 구성을 제외시킴
 					isMax = true;
 				} else if (!isMax) {
-					strs[index++] = key;
+					strs[index] = key;
 				}
+				index++;
 			}
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		if (isMax)
-			return findToMap(map);
+	}
 
-		int max = 0;
-		for (int i = 0; i < strs.length; i++) {
-			char[] chars = strs[i].toCharArray();
-			for (int j = i + 1; j < strs.length; j++) {
-				char[] tempChars = strs[j].toCharArray();
-				int count = 0;
-				for (int t = 0; t < chars.length; t++) {
-					if (chars[t] == tempChars[t]) {
-						count++;
-					}
+	private String sortedString(String readLine) {
+		char[] temp = readLine.replace(" ", "").toCharArray();
+		Arrays.sort(temp);
+		return String.valueOf(temp);
+	}
 
-					if (10 - t + (count - 1) < max) {
-						break;
+	private int buildMap(Map<String, List<Integer>> map, String key, int index) {
+		if (map.get(key) == null) {
+			map.put(key, new ArrayList<>());
+		}
+		List<Integer> values = map.get(key);
+		values.add(index);
+		return values.size();
+	}
+
+	private String findToMap(Map<String, List<Integer>> map) {
+		StringJoiner sj = new StringJoiner(", ");
+		for (Entry<String, List<Integer>> entry : map.entrySet()) {
+			List<Integer> list = entry.getValue();
+			if (entry.getValue().size() > 1) { // map에서 정렬된 문자를 key로 index를 value로 넣어뒀기 때문에, 2개 이상일 때 같은 문자가 있다고 판단
+				for (int i = 0; i < list.size(); i++) {
+					for (int j = i + 1; j < list.size(); j++) {
+						StringBuilder sb = new StringBuilder();
+						sb.append(String.valueOf(list.get(i))).append("-").append(String.valueOf(list.get(j)));
+						sj.add(sb.toString());
 					}
 				}
+			}
+		}
+		return sj.toString();
+	}
+
+	private String findToArray() {
+		int max = 0;
+		StringJoiner sj = new StringJoiner("\n");
+		for (int i = 0; i < strs.length; i++) {
+			char[] left = strs[i].toCharArray();
+			for (int j = i + 1; j < strs.length; j++) {
+				char[] right = strs[j].toCharArray();
+				int count = compair(left, right, max);
 
 				if (count >= max) {
 					if (count > max) {
-						result.clear();
+						sj = new StringJoiner("\n"); // 기존에 찾은 문자열보다 일치 문자가 더 많기 때문에 초기화
 						max = count;
 					}
 					StringBuilder sb = new StringBuilder();
-					sb.append(String.valueOf(i + 1)).append("-").append(String.valueOf(j + 1));
-					result.add(sb.toString());
+					sb.append(String.valueOf(i + 2)).append("-").append(String.valueOf(j + 2));
+					sj.add(sb.toString());
 				}
 			}
 		}
-
-		return String.valueOf(result.size());
+		return sj.toString();
 	}
 
-	private static String findToMap(Map<String, List<Integer>> map) {
-		List<String> result = new ArrayList<>();
-		for (Entry<String, List<Integer>> entry : map.entrySet()) {
-			List<Integer> list = entry.getValue();
-			if (entry.getValue().size() > 1) {
-				for (int i = 0; i < list.size(); i++) {
-					for (int j = i + 1; j < list.size(); j++) {
-						StringBuilder sb = new StringBuilder();
-						sb.append(String.valueOf(list.get(i))).append("-").append(String.valueOf(list.get(j)));
-						result.add(sb.toString());
-					}
-				}
+	private int compair(char[] left, char[] right, int max) {
+		int index = 0, count = 0;
+		for (int i = 0; i < left.length && index < right.length; i++, index++) {
+			if (left[i] == right[index]) {
+				count++;
+			} else if (left[i] > right[index]) { // 정렬이 되어 있기 때문에 불 일치 할 경우 작은 문자열만 오른쪽으로 이동
+				i--;
+			} else { // 정렬이 되어 있기 때문에 불 일치 할 경우 작은 문자열만 오른쪽으로 이동
+				index--;
+			}
+
+			if (left.length - (Math.max(i, index) + 1) + count < max) { // 비교 뭄자열이 일치하는 개수보다 적을경우
+				return 0;
 			}
 		}
-		return String.valueOf(result.size());
-	}
-
-	public static String build3() {
-		// 정렬 안된 문자열을 배열로 만들고, 정렬된 문자열을 key로, 정렬했을 때 같은 문자들의 index를 배열로..
-		List<String> result = new ArrayList<>();
-		Map<String, List<Integer>> map = new HashMap<>();
-		try (BufferedReader br = new BufferedReader(new FileReader(NAME_500000))) {
-			br.readLine();
-			// StringJoiner sj = new StringJoiner("\n");
-			int index = 1;
-			while (br.ready()) {
-				char[] temp = br.readLine().replace(" ", "").toCharArray();
-				Arrays.sort(temp);
-				String key = String.valueOf(temp);
-				if (map.get(key) == null) {
-					map.put(key, new ArrayList<>());
-				}
-				map.get(key).add(index++);
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-		for (Entry<String, List<Integer>> entry : map.entrySet()) {
-			List<Integer> list = entry.getValue();
-			if (entry.getValue().size() > 1) {
-				for (int i = 0; i < list.size(); i++) {
-					for (int j = i + 1; j < list.size(); j++) {
-						StringBuilder sb = new StringBuilder();
-						sb.append(String.valueOf(list.get(i))).append("-").append(String.valueOf(list.get(j)));
-						// sj.add(sb);
-						result.add(String.valueOf(sb));
-					}
-				}
-			}
-		}
-
-		return String.valueOf(result.size());
-	}
-
-	public static boolean stringEmptyChk(String str) {
-		return str != null && !"".equals(str);
+		return count;
 	}
 
 	public static void main(String[] args) throws IOException {
-		 long start1 = System.currentTimeMillis();
-		 System.out.println(CoupleMatching.build1());
-		 System.out.println((System.currentTimeMillis() - start1) / 1000.0);
-		 System.out.println("==============================================");
-		
-		 long start2 = System.currentTimeMillis();
-		 System.out.println(CoupleMatching.build2());
-		 System.out.println((System.currentTimeMillis() - start2) / 1000.0);
-		 System.out.println("==============================================");
-		
-		 long start3 = System.currentTimeMillis();
-		 System.out.println(CoupleMatching.build3());
-		 System.out.println((System.currentTimeMillis() - start3) / 1000.0);
+		String path = CoupleMatching.class.getResource("").getPath();
+		String[] fileNames = new String[] { "100.txt", "10000.txt", "500000.txt" };
+		for (String fileName : fileNames) {
+			CoupleMatching c = new CoupleMatching();
+			String result = c.build(path + fileName);
+			System.out.println(result);
+			System.out.println(path + "OUTPUT_" + fileName);
+			try (BufferedWriter writer = new BufferedWriter(new FileWriter(path + "OUTPUT_" + fileName))) {
+				writer.write(result);
+			}
+		}
 	}
 }
