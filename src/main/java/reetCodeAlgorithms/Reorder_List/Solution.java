80cc6ed1f986c71b74cd35e60344617e36bbbc21
정재공 <jjkong86@gmail.com>
2020-01-27 18:51:14 +0900

    leetcode : Reorder List 시간복잡도 최적화 필요함

--- src/main/java/reetCodeAlgorithms/Reorder_List/Solution.java
+++ src/main/java/reetCodeAlgorithms/Reorder_List/Solution.java
@@ -3,35 +3,54 @@ package reetCodeAlgorithms.Reorder_List;
 import reetCodeAlgorithms.ListNode;
 
 class Solution {
+
+	class Pair {
+		int count;
+		ListNode node;
+
+		Pair(int count, ListNode node) {
+			this.count = count;
+			this.node = node;
+		}
+	}
+
+	// 1 > 2 > 3 > 4 > 5 > 6
+	// 1 > 6 > 2 > 5 > 3 > 4
 	public void reorderList(ListNode head) {
-		ListNode save = head;
-		ListNode tail = findTail(head);
-		ListNode prevNode = null;
-		while (tail != prevNode) {
-			prevNode = head;
+		if (head == null)
+			return;
+		// 1 > 5 > 2, 2 > 4 > 3,
+		ListNode save = head, tail = null;
+		Pair pair = findTail(save);
+		int count = pair.count / 2;
+		tail = pair.node;
+
+		while (count-- > 0) {
 			ListNode headNext = head.next;
 			ListNode tailNext = tail.next;
-			head.next = tail;
-			if (tail.next == head) {
-				tail.next = null;
-				head = save;
-				break;
+			if (count != 0 || count == 0 && pair.count % 2 != 0) {
+				head.next = tail;
 			}
-			tail.next = head.next;
+
+			tail.next = headNext;
 			head = headNext;
 			tail = tailNext;
 		}
-		head = save;
+		head.next = null;
 	}
 
-	private ListNode findTail(ListNode head) {
-		ListNode res = null;
+	private Pair findTail(ListNode head) {
+		ListNode tail = null;
+		int count = 0;
 		while (head != null) {
-			ListNode next = head.next;
-			head.next = res;
-			res = head;
-			head = next;
+			ListNode temp = new ListNode(head.val);
+			temp.next = tail;
+			tail = temp;
+			head = head.next;
+			count++;
 		}
-		return res;
+
+		return new Pair(count, tail);
 	}
+
 }
