e8066d27f21ba3310fef1d02d1b28b0259beae26
정재공 <jjkong86@gmail.com>
2020-05-20 22:32:35 +0900

    leetCode : Path Sum dfs로 풀었고, leaf노드 일때만 조건을 만족함

--- src/main/java/reetCodeAlgorithms/Path_Sum/Solution.java
+++ src/main/java/reetCodeAlgorithms/Path_Sum/Solution.java
@@ -3,31 +3,44 @@ package reetCodeAlgorithms.Path_Sum;
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode(int x) { val = x; }
  * }
  */
 class Solution {
-	
-	public static class TreeNode {
-		int val;
-		TreeNode left;
-		TreeNode right;
-		TreeNode(int x) { val = x; }
-	}
-	
-    public boolean hasPathSum(TreeNode root, int sum) {
-        // root ~ leef node 까지 합계 == sum 경우를 찾아야함
-        if (root == null) return false;
-        
-        return findSum(root, sum, 0);
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
     }
-    
-    public boolean findSum(TreeNode node, int sum, int tempSum) {
-        if (node == null) return sum == tempSum;
-        return findSum(node.left, sum, tempSum + node.val) || 
-            findSum(node.right, sum, tempSum + node.val);
+
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null)
+            return false;
+        return findSum(root, 0, sum);
+    }
+
+    public boolean findSum(TreeNode node, int adding, int sum) {
+        if (node == null)
+            return false;
+
+        adding += node.val;
+
+        if (isLeefNode(node) && adding == sum)
+            return true;
+
+        return findSum(node.left, adding, sum) || findSum(node.right, adding, sum);
+    }
+
+    public boolean isLeefNode(TreeNode node) {
+        return node.left == null && node.right == null;
     }
 }
+
